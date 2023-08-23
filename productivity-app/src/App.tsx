f78f58035d9182cece31b45c8f6b6044f4e313f9
Manuel <manuelpinedacabeza@gmail.com>
2023-08-23 18:30:37 +0200

    Removed  hardcoded learnings and items from WeeklyLearnings

--- productivity-app/src/App.tsx
+++ productivity-app/src/App.tsx
@@ -3,14 +3,9 @@ import "./App.css";
 import WeeklyLearnings from "./components/WeeklyLearnings/WeeklyLearnings";
 
 function App() {
-  const learnings = [
-    { learningText: "Exercise more", learningValue: 3 },
-    { learningText: "Don't drink cofffee in the mornings", learningValue: 5 },
-  ];
-
   return (
     <div className='App'>
-      <WeeklyLearnings items={learnings} />
+      <WeeklyLearnings />
     </div>
   );
 }
