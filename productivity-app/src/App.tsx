4ccc60baa7afc4fed425d23aace26fa16e5853b8
Manuel <manuelpinedacabeza@gmail.com>
2023-08-25 21:47:59 +0200

    Added ToDos component. Imported and added BrowserRouter

--- productivity-app/src/App.tsx
+++ productivity-app/src/App.tsx
@@ -1,13 +1,20 @@
-import "./App.css";
+import "./App.css"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
-import WeeklyLearnings from "./components/WeeklyLearnings/WeeklyLearnings";
+import WeeklyLearnings from "./components/WeeklyLearnings/WeeklyLearnings"
+import ToDos from "./components/ToDos/ToDos"
 
 function App() {
   return (
-    <div className="App">
-      <WeeklyLearnings />
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<WeeklyLearnings />} />
+          <Route path="/todos" element={<ToDos />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
