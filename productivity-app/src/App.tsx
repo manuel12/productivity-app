15ff34742af1aced67bb69a3c9b9c20b5c4b6753
Manuel <manuelpinedacabeza@gmail.com>
2023-08-29 17:06:04 +0200

    Added dailies component and route.

--- productivity-app/src/App.tsx
+++ productivity-app/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import WeeklyLearnings from "./components/WeeklyLearnings/WeeklyLearnings"
 import ToDos from "./components/ToDos/ToDos"
+import Dailies from "./components/Dailies/Dailies"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<WeeklyLearnings />} />
           <Route path="/todos" element={<ToDos />} />
+          <Route path="/dailies" element={<Dailies />} />
         </Routes>
       </div>
     </Router>
