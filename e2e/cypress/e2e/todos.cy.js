91d4065e9e3b34fc4df91bcfd8406373e9c7c1c7
Manuel <manuelpinedacabeza@gmail.com>
2023-09-03 17:32:01 +0200

    Use data attributes as locators

--- e2e/cypress/e2e/todos.cy.js
+++ e2e/cypress/e2e/todos.cy.js
@@ -6,45 +6,47 @@ describe("To Dos tests", () => {
   })
 
   it("should have a 'To Dos' heading", () => {
-    cy.get("h1").should("have.class", "display-1").and("have.text", "To Dos")
+    cy.get("[data-test=todos-heading]")
+      .should("have.class", "display-1")
+      .and("have.text", "To Dos")
   })
 
   it("should display a form with an input", () => {
-    cy.get("form").within(() => {
-      cy.get("input").should("be.visible")
+    cy.get("[data-test=todos-form]").within(() => {
+      cy.get("[data-test=todos-input]").should("be.visible")
     })
   })
 
   it("should display placeholder 'Add a new todo' on input", () => {
-    cy.get("input")
+    cy.get("[data-test=todos-input]")
       .should("be.visible")
       .and("have.attr", "placeholder", "Add a new todo...")
   })
 
   it("should add a new todo by writing on the input and pressing ENTER", () => {
-    cy.get("input").type("Clean room{enter}")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
 
-    cy.get(".list-group-item").should("be.visible")
+    cy.get("[data-test=todos-item]").should("be.visible")
   })
 
   it("should display new todo below the input form", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get(".list-group-item").should("be.visible")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]").should("be.visible")
   })
 
   it("should display correct text on todo", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
       .should("be.visible")
       .and("contain.text", "Clean room")
   })
 
   it("should display checkmark and remove buttons on todo", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
       .should("be.visible")
       .within(($el) => {
-        cy.get(".check-icon-container")
+        cy.get("[data-test=todos-check-icon-container]")
           .should("be.visible")
           .within(() => {
             cy.get(".fa-circle-check").should("be.visible")
@@ -58,11 +60,11 @@ describe("To Dos tests", () => {
   })
 
   it("should change checkmark color on todo when clicked", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
       .should("be.visible")
       .within(() => {
-        cy.get(".check-icon-container")
+        cy.get("[data-test=todos-check-icon-container]")
           .should("be.visible")
           .within(() => {
             cy.get(".fa-circle-check")
@@ -78,24 +80,24 @@ describe("To Dos tests", () => {
   })
 
   it("should display a strike-through on text when checkmark is clicked", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
       .should("be.visible")
       .within(() => {
-        cy.get(".text-container")
+        cy.get("[data-test=todos-text-container]")
           .should("be.visible")
           .and("have.class", "text-not-completed")
         cy.get(".fa-circle-check").click()
 
-        cy.get(".text-container")
+        cy.get("[data-test=todos-text-container]")
           .should("be.visible")
           .and("have.class", "text-completed")
       })
   })
 
   it("should remove todo when remove button on todo is clicked", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
       .should("be.visible")
       .within(() => {
         cy.get(".fa-xmark").should("be.visible")
@@ -103,18 +105,18 @@ describe("To Dos tests", () => {
         cy.get(".fa-xmark").click()
       })
 
-    cy.get(".list-group-item").should("not.exist")
+    cy.get("[data-test=todos-item]").should("not.exist")
   })
 
   it("should add 2 todos", () => {
-    cy.get("input").type("Clean room{enter}")
-    cy.get("input").type("Make lunch{enter}")
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-input]").type("Make lunch{enter}")
 
-    cy.get("ul > :nth-child(1)")
+    cy.get("[data-test=todos-list] > :nth-child(1)")
       .should("be.visible")
       .and("contain.text", "Clean room")
 
-    cy.get("ul > :nth-child(2)")
+    cy.get("[data-test=todos-list] > :nth-child(2)")
       .should("be.visible")
       .and("contain.text", "Make lunch")
   })
@@ -123,9 +125,9 @@ describe("To Dos tests", () => {
     let todoStr = "Clean room"
     for (let i = 0; i < 10; i++) {
       cy.log(i)
-      cy.get("input").type(`${todoStr} ${i + 1} {enter}`)
+      cy.get("[data-test=todos-input]").type(`${todoStr} ${i + 1} {enter}`)
     }
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-item]")
       .should("have.length", 10)
       .each(($el, i) => {
         cy.get($el).should("contain.text", `Clean room ${i + 1}`)
@@ -133,9 +135,9 @@ describe("To Dos tests", () => {
   })
 
   it("should NOT add a todo with empty text when pressing enter", () => {
-    cy.get("input").type("{enter}")
+    cy.get("[data-test=todos-input]").type("{enter}")
 
-    cy.get(".list-group-item").should("not.exist")
+    cy.get("[data-test=todos-item]").should("not.exist")
   })
 
   it("should NOT add a todo with a text more than 40 characters long when pressing enter", () => {
@@ -145,11 +147,11 @@ describe("To Dos tests", () => {
     for (let i = 0; i <= 50; i++) {
       longTodoStr += "A"
     }
-    cy.get("input").type(`${longTodoStr} {enter}`)
-    cy.get(".list-group-item")
+    cy.get("[data-test=todos-input]").type(`${longTodoStr} {enter}`)
+    cy.get("[data-test=todos-item]")
       .should("be.visible")
       .within(() => {
-        cy.get(".text-container")
+        cy.get("[data-test=todos-text-container]")
           .should("be.visible")
           .invoke("text")
           .should("have.length", expectedLength)
