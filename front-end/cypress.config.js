3da0627ff12b607e7c08b9831daf42b91596f3f3
Manuel <manuelpinedacabeza@gmail.com>
2023-09-08 12:29:36 +0200

    Added cypress config to front-end folder

---
+++ front-end/cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress")
+const {
+  addMatchImageSnapshotPlugin,
+} = require("@simonsmith/cypress-image-snapshot/plugin")
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      addMatchImageSnapshotPlugin(on)
+    },
+    baseUrl: "http://localhost:3000/",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+})
