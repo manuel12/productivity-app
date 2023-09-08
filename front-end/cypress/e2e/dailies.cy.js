3a47f924686eb6cd2e8ce82a41cb602bb24d8498
Manuel <manuelpinedacabeza@gmail.com>
2023-09-08 12:29:53 +0200

    Added cypress folder to front-end

---
+++ front-end/cypress/e2e/dailies.cy.js
@@ -0,0 +1,228 @@
+/// <reference types="cypress" />
+
+describe("To Dos tests", () => {
+  beforeEach(() => {
+    cy.visit("/dailies")
+  })
+
+  it("should have a 'Dailies' heading", () => {
+    cy.get("[data-test=dailies-heading]")
+      .should("have.class", "display-1")
+      .and("have.text", "Dailies")
+  })
+
+  it("should display a form with an input", () => {
+    cy.get("[data-test=dailies-form]").within(() => {
+      cy.get("[data-test=dailies-input]").should("be.visible")
+    })
+  })
+
+  it("should display placeholder 'Add a new daily' on input", () => {
+    cy.get("[data-test=dailies-input]")
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Add a new daily...")
+  })
+
+  it("should add a new daily by writing on the input and pressing ENTER", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+
+    cy.get("[data-test=dailies-item]").should("be.visible")
+  })
+
+  it("should display new daily below the input form", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]").should("be.visible")
+  })
+
+  it("should display correct text on daily", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .and("contain.text", "Clean room")
+  })
+
+  it("should display checkmark and remove buttons on daily", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .within(($el) => {
+        cy.get("[data-test=dailies-check-icon-container]")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-circle-check").should("be.visible")
+          })
+        cy.get(".remove-icon-container")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-xmark").should("be.visible")
+          })
+      })
+  })
+
+  it("should change checkmark color on daily when clicked", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get("[data-test=dailies-check-icon-container]")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-circle-check")
+              .should("be.visible")
+              .and("have.class", "check-not-completed")
+            cy.get(".fa-circle-check").click()
+
+            cy.get(".fa-circle-check")
+              .should("be.visible")
+              .and("have.class", "check-completed")
+          })
+      })
+  })
+
+  it("should display a strike-through on text when checkmark is clicked", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get("[data-test=dailies-text-container]")
+          .should("be.visible")
+          .and("have.class", "text-not-completed")
+        cy.get(".fa-circle-check").click()
+
+        cy.get("[data-test=dailies-text-container]")
+          .should("be.visible")
+          .and("have.class", "text-completed")
+      })
+  })
+
+  it("should remove daily when remove button on daily is clicked", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get(".fa-xmark").should("be.visible")
+
+        cy.get(".fa-xmark").click()
+      })
+
+    cy.get("[data-test=dailies-item]").should("not.exist")
+  })
+
+  it("should add 2 dailies", () => {
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-input]").type("Make lunch{enter}")
+
+    cy.get("ul > :nth-child(1)")
+      .should("be.visible")
+      .and("contain.text", "Clean room")
+
+    cy.get("ul > :nth-child(2)")
+      .should("be.visible")
+      .and("contain.text", "Make lunch")
+  })
+
+  it("should add 10 dailies", () => {
+    let dailyStr = "Clean room"
+    for (let i = 0; i < 10; i++) {
+      cy.get("[data-test=dailies-input]").type(`${dailyStr} ${i + 1} {enter}`)
+    }
+    cy.get("[data-test=dailies-item]")
+      .should("have.length", 10)
+      .each(($el, i) => {
+        cy.get($el).should("contain.text", `Clean room ${i + 1}`)
+      })
+  })
+
+  it("should have completed dailies set to uncompleted on next days", () => {
+    // cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    // cy.get("[data-test=dailies-input]").type("Make lunch{enter}")
+    // cy.get("[data-test=dailies-item]").each(($el) => {
+    //   cy.get($el)
+    //     .should("be.visible")
+    //     .within(() => {
+    //       cy.get(".fa-circle-check").click()
+    //       cy.get(".fa-circle-check")
+    //         .should("be.visible")
+    //         .and("have.class", "check-completed")
+    //     })
+    // })
+    // const dateToTestDailiesStr = "2023-09-15T12:00:00Z"
+    // const dateToTestDailies = new Date(dateToTestDailiesStr)
+    // cy.window().then((win) => {
+    //   win.Date = class extends Date {
+    //     constructor() {
+    //       super(dateToTestDailies)
+    //     }
+    //   }
+    // })
+    // cy.reload()
+    // cy.get("[data-test=dailies-item]").each(($el) => {
+    //   cy.get($el)
+    //     .should("be.visible")
+    //     .within(() => {
+    //       cy.get(".fa-circle-check")
+    //         .should("be.visible")
+    //         .and("have.class", "check-not-completed")
+    //     })
+    // })
+  })
+
+  it("should have streak counter value increased +1 when completing daily", () => {
+    let initialStreakCounterValue
+    let secondaryStreakCounterValue
+
+    cy.get("[data-test=dailies-input]").type("Clean room{enter}")
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get(".streak-icon-container")
+          .invoke("text")
+          .then((text) => {
+            initialStreakCounterValue = Number(text)
+            expect(initialStreakCounterValue).to.eq(0)
+          })
+
+        cy.get("[data-test=dailies-check-icon-container]")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-circle-check")
+              .should("be.visible")
+              .and("have.class", "check-not-completed")
+            cy.get(".fa-circle-check").click()
+          })
+
+        cy.get(".streak-icon-container")
+          .invoke("text")
+          .then((text) => {
+            secondaryStreakCounterValue = Number(text)
+            expect(secondaryStreakCounterValue).to.eq(
+              initialStreakCounterValue + 1
+            )
+          })
+      })
+  })
+
+  it("should NOT add a daily with empty text when pressing enter", () => {
+    cy.get("[data-test=dailies-input]").type("{enter}")
+
+    cy.get("[data-test=dailies-item]").should("not.exist")
+  })
+
+  it("should NOT add a daily with a text more than 40 characters long when pressing enter", () => {
+    const expectedLength = 40
+
+    let longDailyStr = "A"
+    for (let i = 0; i <= 50; i++) {
+      longDailyStr += "A"
+    }
+    cy.get("[data-test=dailies-input]").type(`${longDailyStr} {enter}`)
+    cy.get("[data-test=dailies-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get("[data-test=dailies-text-container]")
+          .should("be.visible")
+          .invoke("text")
+          .should("have.length", expectedLength)
+      })
+  })
+})
---
+++ front-end/cypress/e2e/todos.cy.js
@@ -0,0 +1,160 @@
+/// <reference types="cypress" />
+
+describe("To Dos tests", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("should have a 'To Dos' heading", () => {
+    cy.get("[data-test=todos-heading]")
+      .should("have.class", "display-1")
+      .and("have.text", "To Dos")
+  })
+
+  it("should display a form with an input", () => {
+    cy.get("[data-test=todos-form]").within(() => {
+      cy.get("[data-test=todos-input]").should("be.visible")
+    })
+  })
+
+  it("should display placeholder 'Add a new todo' on input", () => {
+    cy.get("[data-test=todos-input]")
+      .should("be.visible")
+      .and("have.attr", "placeholder", "Add a new todo...")
+  })
+
+  it("should add a new todo by writing on the input and pressing ENTER", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+
+    cy.get("[data-test=todos-item]").should("be.visible")
+  })
+
+  it("should display new todo below the input form", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]").should("be.visible")
+  })
+
+  it("should display correct text on todo", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
+      .should("be.visible")
+      .and("contain.text", "Clean room")
+  })
+
+  it("should display checkmark and remove buttons on todo", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
+      .should("be.visible")
+      .within(($el) => {
+        cy.get("[data-test=todos-check-icon-container]")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-circle-check").should("be.visible")
+          })
+        cy.get(".remove-icon-container")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-xmark").should("be.visible")
+          })
+      })
+  })
+
+  it("should change checkmark color on todo when clicked", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get("[data-test=todos-check-icon-container]")
+          .should("be.visible")
+          .within(() => {
+            cy.get(".fa-circle-check")
+              .should("be.visible")
+              .and("have.class", "check-not-completed")
+            cy.get(".fa-circle-check").click()
+
+            cy.get(".fa-circle-check")
+              .should("be.visible")
+              .and("have.class", "check-completed")
+          })
+      })
+  })
+
+  it("should display a strike-through on text when checkmark is clicked", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get("[data-test=todos-text-container]")
+          .should("be.visible")
+          .and("have.class", "text-not-completed")
+        cy.get(".fa-circle-check").click()
+
+        cy.get("[data-test=todos-text-container]")
+          .should("be.visible")
+          .and("have.class", "text-completed")
+      })
+  })
+
+  it("should remove todo when remove button on todo is clicked", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get(".fa-xmark").should("be.visible")
+
+        cy.get(".fa-xmark").click()
+      })
+
+    cy.get("[data-test=todos-item]").should("not.exist")
+  })
+
+  it("should add 2 todos", () => {
+    cy.get("[data-test=todos-input]").type("Clean room{enter}")
+    cy.get("[data-test=todos-input]").type("Make lunch{enter}")
+
+    cy.get("[data-test=todos-list] > :nth-child(1)")
+      .should("be.visible")
+      .and("contain.text", "Clean room")
+
+    cy.get("[data-test=todos-list] > :nth-child(2)")
+      .should("be.visible")
+      .and("contain.text", "Make lunch")
+  })
+
+  it("should add 10 todos", () => {
+    let todoStr = "Clean room"
+    for (let i = 0; i < 10; i++) {
+      cy.log(i)
+      cy.get("[data-test=todos-input]").type(`${todoStr} ${i + 1} {enter}`)
+    }
+    cy.get("[data-test=todos-item]")
+      .should("have.length", 10)
+      .each(($el, i) => {
+        cy.get($el).should("contain.text", `Clean room ${i + 1}`)
+      })
+  })
+
+  it("should NOT add a todo with empty text when pressing enter", () => {
+    cy.get("[data-test=todos-input]").type("{enter}")
+
+    cy.get("[data-test=todos-item]").should("not.exist")
+  })
+
+  it("should NOT add a todo with a text more than 40 characters long when pressing enter", () => {
+    const expectedLength = 40
+
+    let longTodoStr = "A"
+    for (let i = 0; i <= 50; i++) {
+      longTodoStr += "A"
+    }
+    cy.get("[data-test=todos-input]").type(`${longTodoStr} {enter}`)
+    cy.get("[data-test=todos-item]")
+      .should("be.visible")
+      .within(() => {
+        cy.get("[data-test=todos-text-container]")
+          .should("be.visible")
+          .invoke("text")
+          .should("have.length", expectedLength)
+      })
+  })
+})
