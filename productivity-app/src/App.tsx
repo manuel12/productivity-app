39be0c14eac39daa68def1610d479c21061cffee
Manuel <manuelpinedacabeza@gmail.com>
2023-09-09 15:50:33 +0200

    Added learnings array

--- productivity-app/src/App.tsx
+++ productivity-app/src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import "./App.css";
 
 import WeeklyLearnings from "./components/WeeklyLearnings/WeeklyLearnings";
 
 function App() {
-  const learnings = [{ learningText: "Exercise more", learningValue: 3 }];
+  const learnings = [
+    { learningText: "Exercise more", learningValue: 3 },
+    { learningText: "Don't drink cofffee in the mornings", learningValue: 5 },
+  ];
+
   return (
     <div className='App'>
       <WeeklyLearnings items={learnings} />
