1cf09879e640b3eb2da9643e319ca4fddf254c03
Manuel <manuelpinedacabeza@gmail.com>
2023-09-11 10:58:57 +0200

    Imported CustomPage

--- front-end/src/pages/DailiesPage/DailyPage.tsx
+++ front-end/src/pages/DailiesPage/DailyPage.tsx
@@ -1,7 +1,15 @@
 import React from "react"
+import CustomPage from "../CustomPage/CustomPage"
+import DailiesList from "../../components/DailiesList/DailiesList"
 
-const DailyPage = () => {
-  return <div>DailyPage</div>
+const DailyPage: React.FC = () => {
+  return (
+    <CustomPage
+      headingText="Dailies"
+      dataCyAttr="dailies-heading"
+      listComponent={DailiesList}
+    />
+  )
 }
 
 export default DailyPage
