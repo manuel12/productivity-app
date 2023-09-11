2ed08e47c12d6c7d33d6787f69200c1a602e0b2f
Manuel <manuelpinedacabeza@gmail.com>
2023-09-11 10:56:23 +0200

    Added DailyPage to dailies route

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,9 +2,9 @@ import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import Navbar from "./components/Navbar/Navbar"
-import TodoPage from "./pages/TodosPage/TodoPage"
-import Dailies from "./components/Dailies/Dailies"
-import WeeklyLearnings from "./components/WeeklyLearnings/WeeklyLearnings"
+import TodoPage from "./pages/TodosPage/TodosPage"
+import DailyPage from "./pages/DailiesPage/DailyPage"
+import WeeklyLearnings from "./components/WeeklyLearningsList/WeeklyLearningsList"
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<TodoPage />} />
-          <Route path="/dailies" element={<Dailies />} />
+          <Route path="/dailies" element={<DailyPage />} />
           <Route path="/weekly-learnings" element={<WeeklyLearnings />} />
         </Routes>
       </div>
