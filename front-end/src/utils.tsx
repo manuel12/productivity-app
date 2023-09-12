5fe28f103e564aae3f513d3f3b16224eff3e562b
Manuel <manuelpinedacabeza@gmail.com>
2023-09-12 11:03:50 +0200

    Added console logs comments and updating of dateCreated to function. Also imported Daily interface from DailyList.

--- front-end/src/utils.tsx
+++ front-end/src/utils.tsx
@@ -1,3 +1,5 @@
+import { Daily } from "./components/DailiesList/DailiesList"
+
 /**
  * Stores an item with the specified key in localStorage.
  * @param {string} key - The key to use for storing the item.
@@ -22,13 +24,6 @@ export const getItem = (key: string): any | null => {
   return JSON.parse(item)
 }
 
-interface Daily {
-  completed: boolean
-  dailyText: string
-  dateCreated: Date | string
-  streakCounter: number
-}
-
 /**
  * Checks if it's a new day compared to the dateCreated property of the daily
  * and updates the completed property accordingly.
@@ -45,6 +40,11 @@ export const checkAndUpdateCompletedStatus = (daily: Daily) => {
    */
   const createdDate = new Date(daily.dateCreated)
 
+  console.log("Checking daily: ", daily)
+  console.log(
+    `currentDate: ${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()} - createdDate: ${createdDate.getDate()}/${createdDate.getMonth()}/${createdDate.getFullYear()}`
+  )
+
   // Check if it's a new day
   if (
     currentDate.getDate() !== createdDate.getDate() ||
@@ -53,6 +53,17 @@ export const checkAndUpdateCompletedStatus = (daily: Daily) => {
   ) {
     // It's a new day, update the completed property
     daily.completed = false
+
+    // Set the dateCreated to the currentDate so that user can only complete
+    // the daily once per day and therefore only get 1 streak point for completing
+    // the task each day.
+
+    // Before we had the issue where daily was created on say Jan 1st and user would complete
+    // and get 1st streak point. Then on Jan 2nd daily would appear uncomplete user would complete
+    // and then get 2nd streak point. Then he would reload the page and since Jan 1st !==  Jan 2nd
+    // daily appear uncomplete again and user could complete and get a 3rd streak point on the 2nd
+    // day. Updating dateCreated solves this issue.
+    daily.dateCreated = currentDate
   }
 
   return daily
