8ffc15150e35f86ff682ca509525060a7e0c57f2
Manuel <manuelpinedacabeza@gmail.com>
2023-09-12 11:06:29 +0200

    Imported CustomPage and WeeklyLearningsList

--- front-end/src/pages/WeeklyLearningsPage/WeeklyLearningPage.tsx
+++ front-end/src/pages/WeeklyLearningsPage/WeeklyLearningPage.tsx
@@ -1,7 +1,15 @@
 import React from "react"
+import CustomPage from "../CustomPage/CustomPage"
+import WeeklyLearningsList from "../../components/WeeklyLearningsList/WeeklyLearningsList"
 
-const WeeklyLearningPage = () => {
-  return <div>WeeklyLearningPage</div>
+const WeeklyLearningPage: React.FC = () => {
+  return (
+    <CustomPage
+      headingText="Weekly Learnings"
+      dataCyAttr="learnings-heading"
+      listComponent={WeeklyLearningsList}
+    />
+  )
 }
 
 export default WeeklyLearningPage
