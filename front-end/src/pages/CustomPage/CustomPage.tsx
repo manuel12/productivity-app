72eca1e418e1aed93a9b1cf899f3ce75623bc4eb
Manuel <manuelpinedacabeza@gmail.com>
2023-09-26 21:50:27 +0200

    Removed listComponent and added children prop instead.

--- front-end/src/pages/CustomPage/CustomPage.tsx
+++ front-end/src/pages/CustomPage/CustomPage.tsx
@@ -3,19 +3,21 @@ import React, { ReactNode, ComponentType } from "react"
 interface CustomPageProps {
   headingText: string
   dataCyAttr: string
-  listComponent: ComponentType<any>
+  // listComponent: ComponentType<any>
+  children: ReactNode
 }
 const CustomPage: React.FC<CustomPageProps> = ({
   headingText = "",
   dataCyAttr = "",
-  listComponent: ListComponent,
+  children,
+  // listComponent: ListComponent,
 }) => {
   return (
     <div className="my-5">
       <h1 className="display-1" data-cy={dataCyAttr}>
         {headingText}
       </h1>
-      <ListComponent />
+      {children}
     </div>
   )
 }
