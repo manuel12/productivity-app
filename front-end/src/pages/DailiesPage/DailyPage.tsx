8dff9fc96375b563dc458e11695ebd17a0c00c3c
Manuel <manuelpinedacabeza@gmail.com>
2023-09-26 21:51:01 +0200

    Removed listComponent and added DailiesList as child instead.

--- front-end/src/pages/DailiesPage/DailyPage.tsx
+++ front-end/src/pages/DailiesPage/DailyPage.tsx
@@ -4,11 +4,9 @@ import DailiesList from "../../components/DailiesList/DailiesList"
 
 const DailyPage: React.FC = () => {
   return (
-    <CustomPage
-      headingText="Dailies"
-      dataCyAttr="dailies-heading"
-      listComponent={DailiesList}
-    />
+    <CustomPage headingText="Dailies" dataCyAttr="dailies-heading">
+      <DailiesList />
+    </CustomPage>
   )
 }
 
