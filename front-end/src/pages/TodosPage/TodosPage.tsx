4f7d67e1a6a99782153618fb7da99256942a88be
Manuel <manuelpinedacabeza@gmail.com>
2023-09-27 21:53:09 +0200

    Added Imported react hooks TodoStatistics utils funcs and ITodo interface. Added todos and completedTodos states. Added children to CustomPage.

--- front-end/src/pages/TodosPage/TodosPage.tsx
+++ front-end/src/pages/TodosPage/TodosPage.tsx
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import CustomPage from "../CustomPage/CustomPage"
+import TodoStatistics from "../../components/TodoStatistics/TodoStatistics"
 import TodoList from "../../components/TodosList/TodosList"
+import { getItem, getNumCompletedTodos } from "../../utils"
+import ITodo from "../../interfaces/ITodo"
 
 const TodoPage: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>(getItem("todos") || [])
+  const [completedTodos, setCompletedTodos] = useState(
+    getNumCompletedTodos(todos)
+  )
+
+  useEffect(() => {})
+
   return (
-    <CustomPage
-      headingText="To Dos"
-      dataCyAttr="todos-heading"
-      listComponent={TodoList}
-    />
+    <>
+      <CustomPage headingText="To Dos" dataCyAttr="todos-heading">
+        <TodoStatistics todos={todos} completedTodos={completedTodos} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </CustomPage>
+    </>
   )
 }
 
