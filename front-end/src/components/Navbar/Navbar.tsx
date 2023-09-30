abc648a0c6db48955965d4bc81b276b4c022d61f
Manuel <manuelpinedacabeza@gmail.com>
2023-09-30 22:36:55 +0200

    Updated data attributes

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -10,8 +10,8 @@ const Navbar: React.FC = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
