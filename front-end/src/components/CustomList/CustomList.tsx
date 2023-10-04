a453c1f9bb6eed0d66436b57085cd169e8245e72
Manuel <manuelpinedacabeza@gmail.com>
2023-10-04 19:13:44 +0200

    Completed custom list component

--- front-end/src/components/CustomList/CustomList.tsx
+++ front-end/src/components/CustomList/CustomList.tsx
@@ -1,1 +1,29 @@
-export {}
+import React, { ReactNode } from "react"
+
+interface CustomListProps {
+  items: any
+  itemName: string
+  dataCyAttr: string
+  children: ReactNode
+}
+
+const CustomList: React.FC<CustomListProps> = ({
+  items,
+  itemName = "",
+  dataCyAttr = "",
+  children,
+}) => {
+  return (
+    <div className="mx-auto w-50">
+      <ul className="list-group" data-cy={dataCyAttr}>
+        {items.length > 0 ? (
+          children
+        ) : (
+          <div className="display-4">No {itemName} added yet...</div>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default CustomList
