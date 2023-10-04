a276cf8dd833626e0636a4f1fa4dd2980f857f1a
Manuel <manuelpinedacabeza@gmail.com>
2023-10-04 19:14:54 +0200

    Removed hardcoded input and list and import custom input and list instead.

--- front-end/src/components/TodosList/TodosList.tsx
+++ front-end/src/components/TodosList/TodosList.tsx
@@ -1,8 +1,13 @@
 import "./styles.css"
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
+
 import Todo from "../Todo/Todo"
 import ITodo from "../../interfaces/ITodo"
-import { setItem, getItem } from "../../utils"
+
+import CustomInput from "../CustomInput/CustomInput"
+import CustomList from "../CustomList/CustomList"
+
+import { setItem } from "../../utils"
 
 interface ITodoListProps {
   todos: ITodo[]
@@ -33,60 +38,29 @@ const TodoList: React.FC<ITodoListProps> = ({
 
   return (
     <>
-      <form onSubmit={handleAddTodo} data-cy="todos-form">
-        <div className="input-group my-5 mx-auto w-50">
-          <input
-            style={{
-              borderRadius: "25px",
-              borderWidth: "2px",
-              padding: "7px 15px",
-            }}
-            type="text"
-            className="form-control mx-1"
-            data-cy="todos-input"
-            placeholder="Add a new todo..."
-            aria-label="Add a new todo..."
-            aria-describedby="button-addon2"
-            value={newTodo.text}
-            onChange={(e) => {
-              const inputValue = e.target.value
-              if (inputValue.length <= 40) {
-                setNewTodo({ completed: false, text: e.target.value })
-              }
-            }}
-            maxLength={40}
-          />
+      <CustomInput
+        itemName="todos"
+        handleAddItem={handleAddTodo}
+        newItem={newTodo}
+        onChange={(e) => {
+          const inputValue = e.target.value
+          if (inputValue.length <= 40) {
+            setNewTodo({ completed: false, text: e.target.value })
+          }
+        }}
+      />
 
-          <button
-            style={{
-              borderRadius: "20px",
-            }}
-            className="btn btn-primary"
-            data-cy="todos-submit"
-            type="submit"
-            id="button-addon2"
-          >
-            Add todo
-          </button>
-        </div>
-      </form>
-      <div className="mx-auto w-50">
-        <ul className="list-group" data-cy="todos-list">
-          {todos.length > 0 ? (
-            todos.map((todo, i) => (
-              <Todo
-                index={i}
-                todo={todo}
-                todos={todos}
-                setTodos={setTodos}
-                setCompletedTodos={setCompletedTodos}
-              />
-            ))
-          ) : (
-            <div className="display-4">No todos added yet...</div>
-          )}
-        </ul>
-      </div>
+      <CustomList items={todos} itemName="todos" dataCyAttr="todos-list">
+        {todos.map((todo, i) => (
+          <Todo
+            index={i}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+            setCompletedTodos={setCompletedTodos}
+          />
+        ))}
+      </CustomList>
     </>
   )
 }
