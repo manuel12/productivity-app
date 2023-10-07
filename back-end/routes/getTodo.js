e51e8c4923bf9fd1e351661ca1222305a06aae51
Manuel <manuelpinedacabeza@gmail.com>
2023-10-07 19:19:52 +0200

    Updated to JS file

---
+++ back-end/routes/getTodo.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../database")
+
+/* GET a single todo */
+router.get("/api/todos/:id", function (req, res, next) {
+  const querySQL = "SELECT * FROM Todo WHERE id = ?"
+  const params = [req.params.id]
+
+  db.all(querySQL, params, (err, row) => {
+    if (err) {
+      return res.status(400).json({ error: err.message })
+    }
+    res.json({
+      message: row
+        ? "Todo successfully retrieved!"
+        : `Todo with id: ${params} not found.`,
+      data: row,
+    })
+  })
+})
+
+module.exports = router
