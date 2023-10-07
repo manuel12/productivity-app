7ea610342f5f683539b4d5dce6563cefbdb4a23a
Manuel <manuelpinedacabeza@gmail.com>
2023-10-07 19:20:54 +0200

    Updated comment and return statement

--- back-end/routes/getTodos.js
+++ back-end/routes/getTodos.js
@@ -2,14 +2,13 @@ const express = require("express")
 const router = express.Router()
 const db = require("../database")
 
-/* GET todos listing. */
+/* GET todos listing */
 router.get("/api/todos/", function (req, res, next) {
   const querySQL = "SELECT * FROM Todo"
   const params = []
   db.all(querySQL, params, (err, row) => {
     if (err) {
-      res.status(400).json({ error: err.message })
-      return
+      return res.status(400).json({ error: err.message })
     }
     res.json({
       message: "Todos successfully retrieved!",
