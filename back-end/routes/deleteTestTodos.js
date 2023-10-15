06dab1dc832bbb419462d9ab119023c09c84a366
Manuel <manuelpinedacabeza@gmail.com>
2023-10-15 17:06:24 +0200

    Added delete test todos routes

---
+++ back-end/routes/deleteTestTodos.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../database")
+
+router.delete("/api/todos/delete-test-todos", (req, res, next) => {
+  db.run(
+    `DELETE FROM Todo WHERE description LIKE '%test%'`,
+    function (err, result) {
+      if (err) {
+        return res.status(400).json({ error: res.message })
+      }
+      res.json({ message: "Todos deleted!", rows: this.changes })
+    }
+  )
+})
+
+module.exports = router
