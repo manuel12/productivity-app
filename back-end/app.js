3d78a65bb67b541067c4ef62a6f06c2daadb600c
Manuel <manuelpinedacabeza@gmail.com>
2023-10-15 17:06:03 +0200

    Added updateTodoRouter and deleteTestTodos

--- back-end/app.js
+++ back-end/app.js
@@ -5,7 +5,10 @@ const cors = require("cors")
 const getTodosRouter = require("./routes/getTodos")
 const getTodoRouter = require("./routes/getTodo")
 const createTodoRouter = require("./routes/createTodo")
+const updateTodoRouter = require("./routes/updateTodo")
 const deleteTodoRouter = require("./routes/deleteTodo")
+// WARNING!
+const deleteTestTodos = require("./routes/deleteTestTodos")
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -22,8 +25,14 @@ app.use(getTodoRouter)
 // Create a todo
 app.use(createTodoRouter)
 
+// Update a todo
+app.use(updateTodoRouter)
+
 // Delete a todo
 app.use(deleteTodoRouter)
+
+// Delete test todos
+app.use(deleteTestTodos)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
