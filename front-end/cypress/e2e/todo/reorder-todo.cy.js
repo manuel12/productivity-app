a15eb562ff1256f78fe1474ebc71a3b98f828015
Manuel <manuelpinedacabeza@gmail.com>
2023-10-20 17:13:28 +0200

    Added (test) to todo descriptions. Added afterEach hook.

--- front-end/cypress/e2e/todo/reorder-todo.cy.js
+++ front-end/cypress/e2e/todo/reorder-todo.cy.js
@@ -7,10 +7,10 @@ describe("Todo Section - Reorder Todos", () => {
   })
 
   it("should reorder todos by dragging them up and down and validate the order updates correctly", () => {
-    cy.get("[data-cy=todos-input]").type("First Todo Item") // Type the todo text
+    cy.get("[data-cy=todos-input]").type("First Todo Item (test)") // Type the todo text
     cy.get('[data-cy="todos-submit"]').click() // Click on the "Add" button
 
-    cy.get("[data-cy=todos-input]").type("Second Todo Item")
+    cy.get("[data-cy=todos-input]").type("Second Todo Item (test)")
     cy.get('[data-cy="todos-submit"]').click()
 
     // Find the todo items you want to reorder and drag them using Cypress commands
@@ -26,7 +26,15 @@ describe("Todo Section - Reorder Todos", () => {
     // Validate the order of todos has updated correctly after reordering
     // Check the visual indicators or specific order-related properties of todos
     // For example, you can check the text of todos to verify the order
-    cy.get("[data-cy=todos-item]").eq(0).contains("Second Todo Item") // Assuming 'First Todo Text' is the text of the first todo after reordering
-    cy.get("[data-cy=todos-item]").eq(1).contains("First Todo Item") // Assuming 'Second Todo Text' is the text of the second todo after reordering
+    cy.get("[data-cy=todos-item]").eq(0).contains("Second Todo Item (test)") // Assuming 'First Todo Text' is the text of the first todo after reordering
+    cy.get("[data-cy=todos-item]").eq(1).contains("First Todo Item (test)") // Assuming 'Second Todo Text' is the text of the second todo after reordering
+  })
+
+  afterEach(() => {
+    // Call an API function that
+    cy.request({
+      method: "DELETE",
+      url: "http://localhost:4000/api/todos/delete-test-todos/",
+    })
   })
 })
