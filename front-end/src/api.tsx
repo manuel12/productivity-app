6bb3bb7923ff10505becf03db77ee6176d2ede20
Manuel <manuelpinedacabeza@gmail.com>
2023-10-21 17:15:27 +0200

    Added api class

---
+++ front-end/src/api.tsx
@@ -0,0 +1,93 @@
+import ITodo from "./interfaces/ITodo"
+
+class API {
+  static getTodos(setTodos: any): void {
+    fetch("http://localhost:4000/api/todos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.warn(res.error)
+        } else {
+          console.warn(res.message)
+          setTodos(res.data)
+        }
+      })
+  }
+
+  static getTodo(id: number): void {
+    fetch(`http://localhost:4000/api/todos/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.warn(res.error)
+        } else {
+          console.warn(res.message)
+        }
+      })
+  }
+
+  static addTodo(todoData: ITodo): void {
+    fetch("http://localhost:4000/api/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todoData),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.warn(res.error)
+        } else {
+          console.warn(res.message)
+        }
+      })
+  }
+
+  static editTodo(todoData: ITodo, id: number): void {
+    fetch(`http://localhost:4000/api/todos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todoData),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.warn(res.error)
+        } else {
+          console.warn(res.message)
+        }
+      })
+  }
+
+  static deleteTodo(id: number): void {
+    fetch(`http://localhost:4000/api/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.warn(res.error)
+        } else {
+          console.warn(res.message)
+        }
+      })
+  }
+}
+
+export default API
