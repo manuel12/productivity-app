20e0cc405be89db118f8e3d0172b1aacd23bdd53
Manuel <manuelpinedacabeza@gmail.com>
2023-10-22 20:52:14 +0200

    Renamed getTodo to readTodo.

---
+++ back-end/routes/readTodo.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../database")
+
+router.get("/api/todos/:id", function (req, res, next) {
+  const querySQL = "SELECT * FROM Todo WHERE id = ?"
+  const id = req.params.id
+
+  db.all(querySQL, [id], (err, row) => {
+    if (err) {
+      return res.status(500).json({ error: err.message })
+    }
+
+    if (row.length > 0) {
+      res.status(200).json({
+        message: "Todo successfully retrieved!",
+        data: row,
+      })
+    } else {
+      res.status(400).json({
+        message: `Todo with id ${id} not found.`,
+        data: row,
+      })
+    }
+  })
+})
+
+module.exports = router
