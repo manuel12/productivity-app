201df85e9d6542db9c2bdf7a03d2d4b718f1f7bf
Manuel <manuelpinedacabeza@gmail.com>
2023-10-22 20:51:28 +0200

    Added checks for request body validity and updated error messages and status responses.

--- back-end/routes/deleteTodo.js
+++ back-end/routes/deleteTodo.js
@@ -3,17 +3,42 @@ const router = express.Router()
 const db = require("../database")
 
 router.delete("/api/todos/:id", (req, res, next) => {
-  const id = req.params.id
+  if (req.params.id === "delete-test-todos") return next()
 
-  if (id === "delete-test-todos") return next()
+  const id = Number(req.params.id)
 
-  const insertSQL = "DELETE FROM Todo WHERE id = ?"
+  if (isNaN(id) || id <= 0) {
+    return res.status(400).json({ error: "Invalid (NaN) todo id" })
+  }
 
-  db.run(insertSQL, id, (err) => {
+  const checkTodoExistsSQL = "SELECT * FROM Todo WHERE id = ?"
+  const deleteSQL = "DELETE FROM Todo WHERE id = ?"
+
+  db.all(checkTodoExistsSQL, [id], (err, row) => {
     if (err) {
-      return res.status(500).json({ error: "Internal Server Error" })
+      return res.status(500).json({ error: res.message })
     }
-    res.json({ message: "Todo successfully deleted!", id })
+
+    if (!row) {
+      // Todo with given id does not exist
+      return res.status(400).json({ error: `Todo with id ${id} not found` })
+    }
+
+    // Todo with  specific id exists proceed with deletion
+    db.run(deleteSQL, id, function (err) {
+      if (err) {
+        return res.status(500).json({ error: res.message })
+      }
+
+      // Check if any rows were affected (i.e., if the Todo was deleted)
+      if (this.changes === 0) {
+        return res.status(404).json({ error: `Todo with id ${id} not found` })
+      }
+
+      return res
+        .status(204)
+        .json({ message: "Todo successfully deleted!", id: Number(id) })
+    })
   })
 })
 
