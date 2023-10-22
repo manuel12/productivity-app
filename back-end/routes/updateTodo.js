5e4d99c9fd9f4315967f6c8de09293b7ffd44588
Manuel <manuelpinedacabeza@gmail.com>
2023-10-22 20:51:47 +0200

    Added checks for request body validity and updated error messages and status responses.

--- back-end/routes/updateTodo.js
+++ back-end/routes/updateTodo.js
@@ -3,27 +3,70 @@ const router = express.Router()
 const db = require("../database")
 
 router.patch("/api/todos/:id", (req, res, next) => {
-  const updatedTodo = req.body
+  const id = Number(req.params.id)
 
+  if (isNaN(id) || id <= 0) {
+    return res.status(400).json({ error: "Invalid (NaN) todo id" })
+  }
+
+  const updatedTodo = {
+    completed: req.body.completed,
+    description: req.body.description,
+  }
+
+  const errors = []
+  if (typeof updatedTodo.completed !== "boolean") {
+    errors.push("No completed (boolean) specified")
+  }
+
+  if (
+    typeof updatedTodo.description !== "string" ||
+    updatedTodo.description.length == 0
+  ) {
+    errors.push("No description specified")
+  }
+
+  if (errors.length) {
+    return res.status(400).json({ error: errors.join(", ") + "." })
+  }
+
+  const checkTodoExistsSQL = "SELECT * FROM Todo WHERE id = ?"
   const updateSQL = `UPDATE Todo set 
     completed = coalesce(?,completed), 
-    description = COALESCE(?,description)
+    description = coalesce(?,description)
     WHERE id = ?`
 
-  db.run(
-    updateSQL,
-    [updatedTodo.completed, updatedTodo.description, req.params.id],
-    (err) => {
-      if (err) {
-        return res.status(400).json({ error: res.message })
-      }
-
-      res.json({
-        message: "Todo successfully updated!",
-        data: updatedTodo,
-      })
+  db.all(checkTodoExistsSQL, [id], (err, row) => {
+    if (err) {
+      return res.status(500).json({ error: res.message })
     }
-  )
+
+    if (!row) {
+      // Todo with given id does not exist
+      return res.status(400).json({ error: "Todo not found" })
+    }
+
+    // Todo with  specific id exists proceed with update
+    db.run(
+      updateSQL,
+      [updatedTodo.completed, updatedTodo.description, id],
+      function (err) {
+        if (err) {
+          return res.status(400).json({ error: res.message })
+        }
+
+        // Check if any rows were affected (i.e., if the Todo was updated)
+        if (this.changes === 0) {
+          return res.status(404).json({ error: `Todo with id ${id} not found` })
+        }
+
+        return res.json({
+          message: "Todo successfully updated!",
+          data: updatedTodo,
+        })
+      }
+    )
+  })
 })
 
 module.exports = router
