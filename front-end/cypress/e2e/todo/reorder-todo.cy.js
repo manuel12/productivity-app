80fd59fd282494a0ac322cddb41b3ee664e5cd90
Manuel <manuelpinedacabeza@gmail.com>
2023-10-23 20:56:58 +0200

    Added getBySel function and replace previous get calls. Updated test names using the verify naming convention.

--- front-end/cypress/e2e/todo/reorder-todo.cy.js
+++ front-end/cypress/e2e/todo/reorder-todo.cy.js
@@ -6,28 +6,26 @@ describe("Todo Section - Reorder Todos", () => {
     cy.visit("/")
   })
 
-  it("should reorder todos by dragging them up and down and validate the order updates correctly", () => {
-    cy.get("[data-cy=todos-input]").type("First Todo Item (test)") // Type the todo text
-    cy.get('[data-cy="todos-submit"]').click() // Click on the "Add" button
+  it("verify todos can be reordered by dragging them up and down and the order updates correctly", () => {
+    cy.getBySel("todos-input").type("First Todo Item (test)") // Type the todo text
+    cy.getBySel("todos-submit").click() // Click on the "Add" button
 
-    cy.get("[data-cy=todos-input]").type("Second Todo Item (test)")
-    cy.get('[data-cy="todos-submit"]').click()
+    cy.getBySel("todos-input").type("Second Todo Item (test)")
+    cy.getBySel("todos-submit").click()
 
     // Find the todo items you want to reorder and drag them using Cypress commands
     const dataTransfer = new DataTransfer()
-    cy.get("[data-cy=todos-item]")
-      .first()
-      .trigger("dragstart", { dataTransfer })
+    cy.getBySel("todos-item").first().trigger("dragstart", { dataTransfer })
 
-    cy.get('[data-cy="todos-list"] > :nth-child(2)').trigger("drop", {
+    cy.get("[data-cy=todos-list] > :nth-child(2)").trigger("drop", {
       dataTransfer,
     })
 
     // Validate the order of todos has updated correctly after reordering
     // Check the visual indicators or specific order-related properties of todos
     // For example, you can check the text of todos to verify the order
-    cy.get("[data-cy=todos-item]").eq(0).contains("Second Todo Item (test)") // Assuming 'First Todo Text' is the text of the first todo after reordering
-    cy.get("[data-cy=todos-item]").eq(1).contains("First Todo Item (test)") // Assuming 'Second Todo Text' is the text of the second todo after reordering
+    cy.getBySel("todos-item").eq(0).contains("Second Todo Item (test)") // Assuming 'First Todo Text' is the text of the first todo after reordering
+    cy.getBySel("todos-item").eq(1).contains("First Todo Item (test)") // Assuming 'Second Todo Text' is the text of the second todo after reordering
   })
 
   afterEach(() => {
