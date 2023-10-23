a51a9ab8c7c897acc043e8b1a09e0524693d0124
Manuel <manuelpinedacabeza@gmail.com>
2023-10-23 20:56:04 +0200

    Added getBySel function and replace previous get calls. Updated test names using the verify naming convention.

--- front-end/cypress/e2e/todo/add-todo.cy.js
+++ front-end/cypress/e2e/todo/add-todo.cy.js
@@ -1,11 +1,6 @@
 /// <reference types="cypress" />
 
 describe("Todo Section - Add Todo", () => {
-  beforeEach(() => {
-    // Visit the app or the specific page
-    cy.visit("/")
-  })
-
   const testTodoOne = "Feed the cats (test)"
   const testTodoTwo = "Take out trash (test)"
   const testTodoThree = "Clean room (test)"
@@ -13,36 +8,45 @@ describe("Todo Section - Add Todo", () => {
     "(test) This is a todo item with more than 40 characters, which should not be allowed"
   const todoTextWith40Char = todoTextLongerThan40Char.slice(0, 40)
 
-  it('should add a new todo to the Todo section by clicking "Add todo" button', () => {
-    // Assuming there's an "Add todo" button
-    cy.get("[data-cy=todos-input]").type(testTodoOne) // Type the todo text
+  beforeEach(() => {
+    // Visit the app or the specific page
+    cy.visit("/")
+  })
+
+  it('verify a new todo can be added by clicking "Add todo" button', () => {
+    cy.getBySel("todos-input").type(testTodoOne) // Type the todo text
     cy.get('[data-cy="todos-submit"]').click() // Click on the "Add" button
 
-    // Assuming a new todo item is appended to the todo list
-    cy.get("[data-cy=todos-list]").should("have.length", 1) // Verify a new todo is added
-    cy.get("[data-cy=todos-list]").first().should("contain.text", testTodoOne) // Verify todo text
+    cy.getBySel("todos-item").should("be.visible")
   })
 
-  it("should add a new todo to the Todo section by pressing Enter", () => {
-    // Assuming there's an input field for adding todo
-    cy.get("[data-cy=todos-input]").type(testTodoTwo + "{enter}") // Type todo and press Enter
+  it("verify a new todo can be added by pressing Enter", () => {
+    cy.getBySel("todos-input").type(testTodoTwo + "{enter}") // Type todo and press Enter
 
     // Assuming a new todo item is appended to the todo list
-    cy.get("[data-cy=todos-list]").should("have.length", 1) // Verify another new todo is added
-    cy.get("[data-cy=todos-list]").first().should("contain.text", testTodoTwo) // Verify todo text
+    cy.getBySel("todos-list").should("have.length", 1) // Verify another new todo is added
+    cy.getBySel("todos-list").first().should("contain.text", testTodoTwo) // Verify todo text
 
-    cy.get("[data-cy=todos-input]").type(testTodoThree + "{ENTER}")
+    cy.getBySel("todos-input").type(testTodoThree + "{ENTER}")
 
-    cy.get("[data-cy=todos-item]").should("be.visible")
+    cy.getBySel("todos-item").should("be.visible")
   })
 
-  it("should not allow todos with more than 40 characters", () => {
-    cy.get("[data-cy=todos-input]").type(todoTextLongerThan40Char + "{enter}")
+  it("verify added  todo appears in the todo list", () => {
+    cy.getBySel("todos-input").type(testTodoOne + "{enter}")
+
+    // Assuming a new todo item is appended to the todo list
+    cy.getBySel("todos-list").should("have.length", 1) // Verify a new todo is added
+    cy.getBySel("todos-list").first().should("contain.text", testTodoOne) // Verify todo text
+  })
+
+  it("verify todos cannot be added with more than 40 characters", () => {
+    cy.getBySel("todos-input").type(todoTextLongerThan40Char + "{enter}")
 
     // Assuming the input field is cleared after attempting to add a long todo
-    cy.get("[data-cy=todos-input]").should("have.value", "") // Verify input field is empty
+    cy.getBySel("todos-input").should("have.value", "") // Verify input field is empty
 
-    cy.get("[data-cy=todos-item]")
+    cy.getBySel("todos-item")
       .should("have.length", 1)
       .and("contain.text", todoTextWith40Char) // Verify no new todo is added
   })
