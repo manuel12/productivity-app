99f01b17e12bd9ccc2cfcde24ec7f536275cf6bd
Manuel <manuelpinedacabeza@gmail.com>
2023-10-23 20:56:29 +0200

    Added getBySel function and replace previous get calls. Updated test names using the verify naming convention.

--- front-end/cypress/e2e/todo/delete-todo.cy.js
+++ front-end/cypress/e2e/todo/delete-todo.cy.js
@@ -6,11 +6,11 @@ describe("Todo Section - Delete Todo", () => {
     cy.visit("/")
   })
 
-  it("Should delete a todo and remove it from the todo list", () => {
+  it("verify a todo can be deleted and removed from the todo list", () => {
     // Create a todo with the text 'Todo to Delete' that you want to delete
     const todoTextToDelete = "Todo to Delete (test)"
-    cy.get("[data-cy=todos-input]").type(todoTextToDelete)
-    cy.get('[data-cy="todos-submit"]').click()
+    cy.getBySel("todos-input").type(todoTextToDelete)
+    cy.getBySel("todos-submit").click()
 
     // // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
     cy.contains(todoTextToDelete)
