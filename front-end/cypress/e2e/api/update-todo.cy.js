4dda7bd8fb2d563cda132f89a0c682cd3e2e4048
Manuel <manuelpinedacabeza@gmail.com>
2023-10-24 21:00:54 +0200

    Added and refactored test. Updated test names using the verify naming convention.

--- front-end/cypress/e2e/api/update-todo.cy.js
+++ front-end/cypress/e2e/api/update-todo.cy.js
@@ -1,17 +1,20 @@
 /// <reference types="cypress" />
 
-describe("UPDATE Todo API tests", () => {
+describe("UPDATE Todo - (PATCH) /api/todos/:id", () => {
   const apiUrl = "http://localhost:4000"
   const testTodo = {
     completed: true,
     description: "Run a marathon (test)",
   }
-
   const updatedTestTodo = {
     completed: false,
     description: "Run a half-marathon (test)",
   }
-
+  const invalidDataTestTodo = {
+    completed: 6,
+    description: ["Run a marathon"],
+  }
+  const invalidId = 999999999
   const ctx = {}
 
   beforeEach(() => {
@@ -21,21 +24,58 @@ describe("UPDATE Todo API tests", () => {
       url: `${apiUrl}/api/todos/`,
       body: testTodo,
     }).then((res) => {
-      expect(res.body.message).to.eq(`Todo successfully created!`)
       ctx.todoId = res.body.id
     })
   })
 
   // PATCH  - /api/todos/:id updateTodo
-  it("PATCH - /api/todos/:id - should update a specific todo", () => {
+  it("verify a specific todo can be updated when requesting with valid data", () => {
     cy.request({
       method: "PATCH",
       url: `${apiUrl}/api/todos/${ctx.todoId}`,
       body: updatedTestTodo,
+      failOnStatusCode: false,
     }).then((res) => {
+      console.log(res)
       expect(res.body.message).to.eq(`Todo successfully updated!`)
       expect(res.body.data.completed).to.eq(updatedTestTodo.completed)
       expect(res.body.data.description).to.eq(updatedTestTodo.description)
+    })
+  })
+
+  it("verify a todo is not updated with invalid data", () => {
+    cy.request({
+      method: "PATCH",
+      url: `${apiUrl}/api/todos/${ctx.todoId}`,
+      body: invalidDataTestTodo,
+      failOnStatusCode: false,
+    }).then((res) => {
+      expect(res.status).to.eq(400)
+      expect(res.body.error).to.eq(
+        "No completed (boolean) specified, No description specified."
+      )
+    })
+  })
+
+  it.only("Verify a todo is not updated when requesting with invalid id", () => {
+    // Delete todo with invalid id
+    cy.request({
+      method: "PATCH",
+      url: `${apiUrl}/api/todos/${invalidId}`,
+      body: testTodo,
+      failOnStatusCode: false,
+    }).then((res) => {
+      console.log(res)
+      expect(res.status).to.eq(400)
+      expect(res.body.error).to.eq("Todo not found")
+    })
+  })
+
+  afterEach(() => {
+    // Delete created test todos
+    cy.request({
+      method: "DELETE",
+      url: "http://localhost:4000/api/todos/delete-test-todos/",
     })
   })
 })
