d04d87a8cc5b99481428277674c44e63c447404e
Manuel <manuelpinedacabeza@gmail.com>
2023-10-24 21:01:31 +0200

    Added getBySel

--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("getBySel", (selector) => {
+  cy.get(`[data-cy=${selector}]`)
+})
