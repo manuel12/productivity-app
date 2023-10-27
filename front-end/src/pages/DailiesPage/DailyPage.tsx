be4b87a86cefd57f93096866b2909bb34d6b242c
Manuel <manuelpinedacabeza@gmail.com>
2023-10-27 10:56:47 +0200

    Added dailes completedDailies states and passed props to DailiesList.

--- front-end/src/pages/DailiesPage/DailyPage.tsx
+++ front-end/src/pages/DailiesPage/DailyPage.tsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import CustomPage from "../CustomPage/CustomPage"
 import DailiesList from "../../components/DailiesList/DailiesList"
+import IDaily from "../../interfaces/IDaily"
 
 const DailyPage: React.FC = () => {
+  const [dailies, setDailies] = useState<IDaily[]>([])
+  const [completedDailies, setCompletedDalies] = useState(0)
+
   return (
     <CustomPage headingText="Dailies" dataCyAttr="dailies-heading">
-      <DailiesList />
+      <DailiesList
+        dailies={dailies}
+        setDailies={setDailies}
+        setCompletedDailies={setCompletedDalies}
+      />
     </CustomPage>
   )
 }
