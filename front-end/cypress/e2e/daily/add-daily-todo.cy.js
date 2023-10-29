d05dcbcb8e6e24eeea0fc8332d19659ee2215c95
Manuel <manuelpinedacabeza@gmail.com>
2023-10-29 22:01:30 +0100

    Replaced deleteTestTodos with deleteTestDailies. Updated test names.

--- front-end/cypress/e2e/daily/add-daily-todo.cy.js
+++ front-end/cypress/e2e/daily/add-daily-todo.cy.js
@@ -1,43 +1,54 @@
 /// <reference types="cypress" />
 
 describe("Daily Section - Add Daily Todo", () => {
-  const testTodoOne = "Feed the cats (test)"
-  const testTodoTwo = "Take out trash (test)"
-  const testTodoThree = "Clean room (test)"
+  const testDailyOne = "Feed the cats (test)"
+  const testDailyTwo = "Take out trash (test)"
+  const testDailyThree = "Clean room (test)"
   const todoTextLongerThan40Char =
-    "(test) This is a todo item with more than 40 characters, which should not be allowed"
+    "(test) This is a daily with more than 40 characters, which should not be allowed"
   const todoTextWith40Char = todoTextLongerThan40Char.slice(0, 40)
 
   beforeEach(() => {
     cy.visit("/dailies")
   })
 
-  it("Verify a daily todo can be added by writing on input and clicking the Add Todo button", () => {
-    cy.getBySel("dailies-input").type(testTodoOne)
+  it("Verify a dailies can be added by writing on input and clicking the Add Todo button", () => {
+    cy.getBySel("dailies-input").type(testDailyOne)
     cy.get('[data-cy="dailies-submit"]').click()
 
     cy.getBySel("dailies-item").should("be.visible")
   })
 
-  it("Verify a daily todo can be added by writing on input and pressing enter key", () => {
-    cy.getBySel("dailies-input").type(testTodoTwo + "{enter}")
+  it("Verify a dailies can be added by writing on input and pressing enter key", () => {
+    cy.getBySel("dailies-input").type(testDailyTwo + "{enter}")
 
     cy.getBySel("dailies-list").should("have.length", 1)
-    cy.getBySel("dailies-list").first().should("contain.text", testTodoTwo)
+    cy.getBySel("dailies-list").first().should("contain.text", testDailyTwo)
 
-    cy.getBySel("dailies-input").type(testTodoThree + "{ENTER}")
+    cy.getBySel("dailies-input").type(testDailyThree + "{ENTER}")
 
     cy.getBySel("dailies-item").should("be.visible")
   })
 
-  it("Verify the daily todo appears correctly on the list", () => {
-    cy.getBySel("dailies-input").type(testTodoOne)
+  it("Verify the dailies appears correctly on the list", () => {
+    cy.getBySel("dailies-input").type(testDailyOne)
     cy.getBySel("dailies-submit").click()
 
     cy.getBySel("dailies-item").should("be.visible")
   })
 
-  it("Verify daily todos cannot be added with more than 40 characters", () => {
+  it("Verify added dailies persist after page reload", () => {
+    cy.getBySel("dailies-input").type(testDailyOne + "{enter}")
+
+    cy.getBySel("dailies-list").should("have.length", 1)
+    cy.getBySel("dailies-list").first().should("contain.text", testDailyOne)
+
+    cy.reload()
+    cy.getBySel("dailies-list").should("have.length", 1)
+    cy.getBySel("dailies-list").first().should("contain.text", testDailyOne)
+  })
+
+  it("Verify dailies cannot be added with more than 40 characters", () => {
     cy.getBySel("dailies-input").type(todoTextLongerThan40Char + "{enter}")
 
     cy.getBySel("dailies-input").should("have.value", "")
@@ -50,7 +61,7 @@ describe("Daily Section - Add Daily Todo", () => {
       .and("contain.text", todoTextWith40Char)
   })
 
-  it("Verify daily todos cannot be added with when leaving the input empty", () => {
+  it("Verify dailies cannot be added with when leaving the input empty", () => {
     cy.get('[data-cy="dailies-submit"]').click()
     cy.getBySel("dailies-item").should("not.exist")
 
@@ -59,10 +70,6 @@ describe("Daily Section - Add Daily Todo", () => {
   })
 
   afterEach(() => {
-    // Call an API function that
-    cy.request({
-      method: "DELETE",
-      url: "http://localhost:4000/api/todos/delete-test-todos/",
-    })
+    cy.deleteTestDailies()
   })
 })
