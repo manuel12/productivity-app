881b8e0c96cdf4e3304cb113817e1bbee720321e
Manuel <manuelpinedacabeza@gmail.com>
2023-10-29 21:58:33 +0100

    handleInputChange handleSaveClick handleKeyDown handleDragStart handleDragStart handleDrop

--- front-end/src/components/Daily/Daily.tsx
+++ front-end/src/components/Daily/Daily.tsx
@@ -12,8 +12,9 @@ import {
   faRemove,
   faForward,
 } from "@fortawesome/free-solid-svg-icons"
-import { setItem, getNumCompletedTodos } from "../../utils"
+import { setItem } from "../../utils"
 import IDailyItemProps from "../../interfaces/IDailyItemProps"
+import API from "../../api"
 
 const Daily: React.FC<IDailyItemProps> = ({
   index,
@@ -30,6 +31,20 @@ const Daily: React.FC<IDailyItemProps> = ({
     console.log(daily)
     setCompleted(daily.completed)
   }, [daily])
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEditedDescription(e.target.value)
+  }
+
+  const handleKeyDown = (
+    e: KeyboardEvent<HTMLInputElement>,
+    todoIndex: number
+  ) => {
+    // Check if ENTER key was pressed
+    if ((e as any).key === "Enter") {
+      handleSaveClick(todoIndex)
+    }
+  }
 
   const handleCheckClick = (dailyIndex: number) => {
     const tempDailiesList = dailies
@@ -56,6 +71,35 @@ const Daily: React.FC<IDailyItemProps> = ({
     setItem("dailies", newDailiesListArray)
   }
 
+  const handleEditClick = () => {
+    setIsEditing(true)
+  }
+
+  const handleSaveClick = (dailyIndex: number) => {
+    // Save the edited description and exit edit mode
+    // You can implement the logic to save the edited description here
+    // For example, you can call an API endpoint to update the todo item on the server
+    const tempTodos = dailies
+
+    const dailyToUpdate = dailies[dailyIndex]
+    dailyToUpdate.description = editedDescription
+
+    tempTodos[dailyIndex] = dailyToUpdate
+    setDailies(tempTodos)
+
+    const useDB = true
+    if (useDB) {
+      const todoId = dailyToUpdate.id
+      todoId && API.editTodo(dailyToUpdate, todoId)
+    } else {
+      setItem("todos", tempTodos)
+    }
+
+    // todoToUpdate.completed && playTodoCompletedSound()
+
+    setIsEditing(false)
+  }
+
   const handleRemoveClick = (dailyIndex: number) => {
     const dailyToRemove = dailies[dailyIndex]
     const newDailiesList = dailies.filter(
@@ -77,6 +121,23 @@ const Daily: React.FC<IDailyItemProps> = ({
     let audio = new Audio("remove-todo.mp3")
     audio.volume = 0.2
     audio.play()
+  }
+
+  const handleDragStart = (e: DragEvent<HTMLLIElement>, index: number) => {
+    e.dataTransfer.setData("index", index.toString())
+  }
+
+  const handleDragOver = (e: DragEvent<HTMLLIElement>) => {
+    e.preventDefault()
+  }
+
+  const handleDrop = (e: DragEvent<HTMLLIElement>, targetIndex: number) => {
+    e.preventDefault()
+    const sourceIndex = parseInt(e.dataTransfer.getData("index"))
+    const updatedDailies = [...dailies]
+    const [movedDaily] = updatedDailies.splice(sourceIndex, 1)
+    updatedDailies.splice(targetIndex, 0, movedDaily)
+    setDailies(updatedDailies)
   }
 
   return (
@@ -102,8 +163,23 @@ const Daily: React.FC<IDailyItemProps> = ({
           completed ? "description-completed" : "description-not-completed"
         }`}
         data-cy="dailies-description-container"
+        onClick={handleEditClick} // Enable editing mode when the description is clicked
       >
-        {daily.description}
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedDescription}
+            onChange={handleInputChange}
+            onBlur={() => handleSaveClick(index)} // Save the edited description when the input field loses focus
+            onKeyDown={(e) => {
+              handleKeyDown(e, index)
+            }}
+            data-cy="dailies-text-input"
+            autoFocus // Automatically focus on the input field when editing starts
+          />
+        ) : (
+          daily.description
+        )}
       </div>
 
       <div className="streak-icon-container">
