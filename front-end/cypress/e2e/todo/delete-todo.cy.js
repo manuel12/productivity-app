3719666fa9016ed345f036ded3f0bbf35fdfe3a1
Manuel <manuelpinedacabeza@gmail.com>
2023-10-29 21:55:11 +0100

    Added Verify edited todos persist after page reload. Replaced hardcoded func with deleteTestTodos.

--- front-end/cypress/e2e/todo/delete-todo.cy.js
+++ front-end/cypress/e2e/todo/delete-todo.cy.js
@@ -12,21 +12,35 @@ describe("Todo Section - Delete Todo", () => {
     cy.getBySel("todos-input").type(todoTextToDelete)
     cy.getBySel("todos-submit").click()
 
-    // // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
+    // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
     cy.contains(todoTextToDelete)
       .parent()
       .find(".remove-icon-container")
       .click()
 
-    // // Validate that the todo with text 'Todo to Delete' no longer exists in the todo list
+    // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
+    cy.contains(todoTextToDelete).should("not.exist")
+  })
+
+  it("Verify deleted todos persist after page reload", () => {
+    // Create a todo with the text 'Todo to Delete' that you want to delete
+    const todoTextToDelete = "Todo to Delete (test)"
+    cy.getBySel("todos-input").type(todoTextToDelete)
+    cy.getBySel("todos-submit").click()
+
+    // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
+    cy.contains(todoTextToDelete)
+      .parent()
+      .find(".remove-icon-container")
+      .click()
+
+    cy.reload()
+
+    // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
     cy.contains(todoTextToDelete).should("not.exist")
   })
 
   afterEach(() => {
-    // Call an API function that
-    cy.request({
-      method: "DELETE",
-      url: "http://localhost:4000/api/todos/delete-test-todos/",
-    })
+    cy.deleteTestTodos()
   })
 })
