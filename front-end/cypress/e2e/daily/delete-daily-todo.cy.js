f2f0fece67887621c14c244aeef915967adf68ba
Manuel <manuelpinedacabeza@gmail.com>
2023-10-29 22:01:59 +0100

    Replaced deleteTestTodos with deleteTestDailies. Updated test names.

--- front-end/cypress/e2e/daily/delete-daily-todo.cy.js
+++ front-end/cypress/e2e/daily/delete-daily-todo.cy.js
@@ -11,21 +11,38 @@ describe("Daily Section - Delete Daily Todo", () => {
     cy.getBySel("dailies-input").type(todoTextToDelete)
     cy.getBySel("dailies-submit").click()
 
-    // // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
+    // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
     cy.contains(todoTextToDelete)
       .parent()
       .find(".remove-icon-container")
       .click()
 
-    // // Validate that the todo with text 'Todo to Delete' no longer exists in the todo list
+    // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
+    cy.contains(todoTextToDelete).should("not.exist")
+  })
+
+  it("Verify deleted dailies persist after page reload", () => {
+    // Create a todo with the text 'Todo to Delete' that you want to delete
+    const todoTextToDelete = "Todo to Delete (test)"
+    cy.getBySel("dailies-input").type(todoTextToDelete)
+    cy.getBySel("dailies-submit").click()
+
+    // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
+    cy.contains(todoTextToDelete)
+      .parent()
+      .find(".remove-icon-container")
+      .click()
+
+    // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
+    cy.contains(todoTextToDelete).should("not.exist")
+
+    cy.reload()
+
+    // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
     cy.contains(todoTextToDelete).should("not.exist")
   })
 
   afterEach(() => {
-    // Call an API function that
-    cy.request({
-      method: "DELETE",
-      url: "http://localhost:4000/api/todos/delete-test-todos/",
-    })
+    cy.deleteTestDailies()
   })
 })
