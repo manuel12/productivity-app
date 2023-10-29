0ab73a493deb1e7c2b3c76c918ad561486c7a579
Manuel <manuelpinedacabeza@gmail.com>
2023-10-29 22:01:43 +0100

    Replaced deleteTestTodos with deleteTestDailies. Updated test names.

--- front-end/cypress/e2e/daily/edit-daily-todo.cy.js
+++ front-end/cypress/e2e/daily/edit-daily-todo.cy.js
@@ -23,16 +23,38 @@ describe("Daily Section - Edit Daily Todo", () => {
     cy.getBySel("dailies-text-input").clear()
     cy.getBySel("dailies-text-input").type(`${updatedTodo}{enter}`)
 
-    // Validate the todo is updated correctly
+    // Validate the daily is updated correctly
     cy.contains(createdTodo).should("not.exist")
     cy.contains("[data-cy=dailies-item]", updatedTodo).should("exist")
   })
 
+  it("Verify edited dailies persist after page reload", () => {
+    const createdTodo = "Created Todo (test)"
+    const updatedTodo = "Updated Todo Item (test)"
+
+    // Create a daily
+    cy.getBySel("dailies-input").type(createdTodo)
+    cy.getBySel("dailies-submit").click()
+
+    // Find and edit that existing daily
+    cy.getBySel("dailies-item")
+      .filter(`:contains(${createdTodo})`)
+      .within(() => {
+        cy.getBySel("dailies-description-container").click()
+      })
+
+    cy.getBySel("dailies-text-input").clear()
+    cy.getBySel("dailies-text-input").type(`${updatedTodo}{enter}`)
+
+    // Verify the daily is updated correctly
+    cy.contains(createdTodo).should("not.exist")
+    cy.contains("[data-cy=dailies-item]", updatedTodo).should("exist")
+
+    cy.reload()
+    cy.contains("[data-cy=dailies-item]", updatedTodo).should("exist")
+  })
+
   afterEach(() => {
-    // Call an API function that
-    cy.request({
-      method: "DELETE",
-      url: "http://localhost:4000/api/todos/delete-test-todos/",
-    })
+    cy.deleteTestDailies()
   })
 })
