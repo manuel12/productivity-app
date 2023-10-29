5710a75f6fd645fce5a928a23ffaff57b9dd5bf6
Manuel <manuelpinedacabeza@gmail.com>
2023-10-29 21:56:06 +0100

    Replaced hardcoded func with deleteTestTodos. Added Verify reordered todos persists after a page reload and Verify reordered todos can be edited and update correctly tests.

--- front-end/cypress/e2e/todo/reorder-todo.cy.js
+++ front-end/cypress/e2e/todo/reorder-todo.cy.js
@@ -27,24 +27,86 @@ describe("Todo Section - Reorder Todos", () => {
         dataTransfer,
       })
 
-    // Validate the order of todos has updated correctly after reordering
-    // Check the visual indicators or specific order-related properties of todos
-    // For example, you can check the text of todos to verify the order
+    // Verify the order of todos has updated correctly after reordering
     cy.getBySel("todos-item")
       .filter(`:contains("test")`)
       .eq(0)
-      .contains("Second Todo Item (test)") // Assuming 'First Todo Text' is the text of the first todo after reordering
+      .contains("Second Todo Item (test)")
     cy.getBySel("todos-item")
       .filter(`:contains("test")`)
       .eq(1)
-      .contains("First Todo Item (test)") // Assuming 'Second Todo Text' is the text of the second todo after reordering
+      .contains("First Todo Item (test)")
+  })
+
+  it("Verify reordered todos persists after a page reload", () => {
+    cy.getBySel("todos-input").type("First Todo Item (test)") // Type the todo text
+    cy.getBySel("todos-submit").click() // Click on the "Add" button
+
+    cy.getBySel("todos-input").type("Second Todo Item (test)")
+    cy.getBySel("todos-submit").click()
+
+    // Find the todo items you want to reorder and drag them using Cypress commands
+    const dataTransfer = new DataTransfer()
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(0)
+      .trigger("dragstart", { dataTransfer })
+
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(1)
+      .trigger("drop", {
+        dataTransfer,
+      })
+
+    cy.reload()
+
+    // Verify the order of todos has updated correctly after reordering
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(0)
+      .contains("Second Todo Item (test)")
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(1)
+      .contains("First Todo Item (test)")
+  })
+
+  it("Verify reordered todos can be edited and update correctly", () => {
+    cy.getBySel("todos-input").type("First Todo Item (test)") // Type the todo text
+    cy.getBySel("todos-submit").click() // Click on the "Add" button
+
+    cy.getBySel("todos-input").type("Second Todo Item (test)")
+    cy.getBySel("todos-submit").click()
+
+    // Find the todo items you want to reorder and drag them using Cypress commands
+    const dataTransfer = new DataTransfer()
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(0)
+      .trigger("dragstart", { dataTransfer })
+
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(1)
+      .trigger("drop", {
+        dataTransfer,
+      })
+
+    cy.reload()
+
+    // Verify the order of todos has updated correctly after reordering
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(0)
+      .contains("Second Todo Item (test)")
+    cy.getBySel("todos-item")
+      .filter(`:contains("test")`)
+      .eq(1)
+      .contains("First Todo Item (test)")
   })
 
   afterEach(() => {
-    // Call an API function that
-    cy.request({
-      method: "DELETE",
-      url: "http://localhost:4000/api/todos/delete-test-todos/",
-    })
+    cy.deleteTestTodos()
   })
 })
