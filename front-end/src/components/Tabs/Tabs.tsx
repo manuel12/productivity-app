696cffbc900d4b8b8aefe3378cd5d6c1c40e00c7
Manuel <manuelpinedacabeza@gmail.com>
2023-12-11 23:03:10 +0100

    Added Tabs component

---
+++ front-end/src/components/Tabs/Tabs.tsx
@@ -0,0 +1,81 @@
+import "./styles.css"
+import React from "react"
+import ITodo from "../../interfaces/ITodo"
+
+interface ITabsProps {
+  tabState: any
+  setTabState: (tabState: any) => void
+  setListTodos: (todos: any) => void
+  todos: ITodo[]
+  completedTodos: ITodo[]
+  uncompletedTodos: ITodo[]
+}
+
+const Tabs: React.FC<ITabsProps> = ({
+  tabState,
+  setTabState,
+  setListTodos,
+  todos,
+  completedTodos,
+  uncompletedTodos,
+}) => {
+  return (
+    <ul className="nav nav-tabs my-3 mx-auto" data-cy="todos-tabs">
+      <li className="nav-item">
+        <div
+          className={`nav-link ${tabState.all}`}
+          aria-current="page"
+          onClick={() => {
+            setTabState({
+              all: "active",
+              complete: "",
+              uncomplete: "",
+            })
+            setListTodos(todos)
+          }}
+          data-cy="all-tab"
+        >
+          All
+        </div>
+      </li>
+
+      <li className="nav-item">
+        <div
+          className={`nav-link ${tabState.complete}`}
+          aria-current="page"
+          onClick={() => {
+            setTabState({
+              all: "",
+              complete: "active",
+              uncomplete: "",
+            })
+            setListTodos(completedTodos)
+          }}
+          data-cy="complete-tab"
+        >
+          Completed
+        </div>
+      </li>
+
+      <li className="nav-item">
+        <div
+          className={`nav-link ${tabState.uncomplete}`}
+          aria-current="page"
+          onClick={() => {
+            setTabState({
+              all: "",
+              complete: "",
+              uncomplete: "active",
+            })
+            setListTodos(uncompletedTodos)
+          }}
+          data-cy="uncomplete-tab"
+        >
+          Uncompleted
+        </div>
+      </li>
+    </ul>
+  )
+}
+
+export default Tabs
