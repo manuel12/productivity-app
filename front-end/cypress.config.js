8645fefaf0df818ce6cc4dad9fa1e4cc8df35ff0
Manuel <manuelpinedacabeza@gmail.com>
2023-12-31 16:10:27 +0100

    Updated specPatterns

--- front-end/cypress.config.js
+++ front-end/cypress.config.js
@@ -10,11 +10,13 @@ module.exports = defineConfig({
       addMatchImageSnapshotPlugin(on)
     },
     baseUrl: "http://localhost:3000/",
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    //specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/smoke/todo/smoke.cy.{js,jsx,ts,tsx}",
     excludeSpecPattern: [
       "cypress/e2e/1-getting-started/",
       "cypress/e2e/2-advanced-examples",
       "cypress/e2e/**.cy.js",
+      "cypress/e2e/functional/daily/**.cy.js",
     ],
     experimentalRunAllSpecs: true,
   },
