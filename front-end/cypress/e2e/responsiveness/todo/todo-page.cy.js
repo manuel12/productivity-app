a6e5e448f464e6bfd3300e6aa16728f58c43d4e0
Manuel <manuelpinedacabeza@gmail.com>
2024-01-07 22:23:00 +0100

    Refactored responsiveness tests. Added snapshots and checks for navbar on mobile.

--- front-end/cypress/e2e/responsiveness/todo/todo-page.cy.js
+++ front-end/cypress/e2e/responsiveness/todo/todo-page.cy.js
@@ -1,58 +1,57 @@
 /// <reference types="cypress" />
 
 describe("Responsiveness Section - Todo Page", () => {
-  context("-600px resolution ", () => {
-    beforeEach(() => {
-      cy.viewport(500, 900)
-      cy.visit("/")
-    })
+  const resolutions = [
+    {
+      breakpoint: "<600px",
+      viewportWidthToUse: 414,
+    },
+    {
+      breakpoint: ">600px",
+      viewportWidthToUse: 601,
+    },
+    {
+      breakpoint: ">768px",
+      viewportWidthToUse: 770,
+    },
+    {
+      breakpoint: ">992px",
+      viewportWidthToUse: 1000,
+    },
+    {
+      breakpoint: ">1200px",
+      viewportWidthToUse: 1300,
+    },
+  ]
 
-    it("should be displayed correctly on -600px of width", () => {
-      cy.get(".App").should("be.visible").matchImageSnapshot()
-    })
-  })
+  const isMobile = (resolution) => resolution.viewportWidthToUse < 992
 
-  context(">600px resolution ", () => {
-    beforeEach(() => {
-      cy.viewport(700, 900)
-      cy.visit("/")
-    })
+  resolutions.forEach((resolution) => {
+    context(`${resolution.breakpoint} resolution `, () => {
+      before(() => {
+        cy.viewport(resolution.viewportWidthToUse, 900)
+        cy.visit("/")
+      })
 
-    it("should be displayed correctly on >600px of width", () => {
-      cy.get(".App").should("be.visible").matchImageSnapshot()
-    })
-  })
+      it(`should be displayed correctly on ${resolution.breakpoint} of width`, () => {
+        cy.getBySel("app-navbar")
+          .should("be.visible")
+          .matchImageSnapshot(`Navbar at ${resolution.viewportWidthToUse}`)
 
-  context(">768px resolution ", () => {
-    beforeEach(() => {
-      cy.viewport(800, 900)
-      cy.visit("/")
-    })
+        if (isMobile(resolution)) {
+          // Open the navbar and take snapshot of the menu
+          cy.get(".navbar-toggler").click()
+          cy.getBySel("app-navbar")
+            .should("be.visible")
+            .matchImageSnapshot(
+              `Navbar (opened) at ${resolution.viewportWidthToUse}`
+            )
+        }
 
-    it("should be displayed correctly on >768px of width", () => {
-      cy.get(".App").should("be.visible").matchImageSnapshot()
-    })
-  })
-
-  context(">992px resolution ", () => {
-    beforeEach(() => {
-      cy.viewport(1000, 900)
-      cy.visit("/")
-    })
-
-    it("should be displayed correctly on >992px of width", () => {
-      cy.get(".App").should("be.visible").matchImageSnapshot()
-    })
-  })
-
-  context(">1200px resolution ", () => {
-    beforeEach(() => {
-      cy.viewport(1300, 900)
-      cy.visit("/")
-    })
-
-    it("should be displayed correctly on 1200px of width", () => {
-      cy.get(".App").should("be.visible").matchImageSnapshot()
+        cy.getBySel("todo-page-container")
+          .should("be.visible")
+          .matchImageSnapshot(`Container at ${resolution.viewportWidthToUse}`)
+      })
     })
   })
 })
