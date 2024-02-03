6309c5daa0b0370a4626927fff6908185775ee8d
Manuel <manuelpinedacabeza@gmail.com>
2024-02-03 09:19:24 +0100

    Added LoggedOutRoutes LoggedInRoutes userLoggedIn state and Register and Login pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,38 @@
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { useState } from "react"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom"
+
+import LoginPage from "./pages/LoginPage/LoginPage"
+import RegisterPage from "./pages/RegisterPage/RegisterPage"
 
 import Navbar from "./components/Navbar/Navbar"
 import TodoPage from "./pages/TodosPage/TodosPage"
 import DailyPage from "./pages/DailiesPage/DailyPage"
 import WeeklyLearningPage from "./pages/WeeklyLearningsPage/WeeklyLearningPage"
+import { getItem } from "./utils"
 
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(getItem("userLoggedIn"))
+
+  return (
+    <div className="App">
+      {userLoggedIn ? (
+        <LoggedInRoutes />
+      ) : (
+        <LoggedOutRoutes setLogin={setUserLoggedIn} />
+      )}
+    </div>
+  )
+}
+
+export default App
+
+const LoggedInRoutes = () => {
   return (
     <Router>
       <div className="App">
@@ -15,10 +41,28 @@ function App() {
           <Route path="/" element={<TodoPage />} />
           <Route path="/dailies" element={<DailyPage />} />
           <Route path="/weekly-learnings" element={<WeeklyLearningPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
   )
 }
 
-export default App
+interface LoggedOutRoutesProps {
+  setLogin: any
+}
+
+const LoggedOutRoutes: React.FC<LoggedOutRoutesProps> = ({ setLogin }) => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/account/login"
+          element={<LoginPage setLogin={setLogin} />}
+        />
+        <Route path="/account/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/account/login" replace />} />
+      </Routes>
+    </Router>
+  )
+}
