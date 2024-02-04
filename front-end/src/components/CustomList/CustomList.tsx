2f00ba7d9ad3e75dfb9e9bbaa8f4a8f181ebb55e
Manuel <manuelpinedacabeza@gmail.com>
2024-02-04 09:22:12 +0100

    Updated css class names

--- front-end/src/components/CustomList/CustomList.tsx
+++ front-end/src/components/CustomList/CustomList.tsx
@@ -16,7 +16,10 @@ const CustomList: React.FC<CustomListProps> = ({
 }) => {
   return (
     <div className="mx-auto mb-5">
-      <ul className="list-group mx-auto" data-cy={dataCyAttr}>
+      <ul
+        className="list-group mx-auto CL__unordered-list"
+        data-cy={dataCyAttr}
+      >
         {items.length < 1 ? (
           <div className="display-4">No {itemName} added yet...</div>
         ) : (
