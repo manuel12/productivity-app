af2e025d75bb9b3a7670ec822b12699b224c81d5
Manuel <manuelpinedacabeza@gmail.com>
2024-02-05 09:26:30 +0100

    Added RegisterPage component

---
+++ front-end/src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import RegisterForm from "../../forms/RegisterForm/RegisterForm"
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  )
+}
+
+export default RegisterPage
