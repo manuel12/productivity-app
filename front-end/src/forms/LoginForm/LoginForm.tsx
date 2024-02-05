18c3f20766ff8d245636402cc730fa4a479ed2f2
Manuel <manuelpinedacabeza@gmail.com>
2024-02-05 09:26:04 +0100

    Added  forms folder

---
+++ front-end/src/forms/LoginForm/LoginForm.tsx
@@ -0,0 +1,196 @@
+import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import LabeledInput from "../../components/LabeledInput/LabeledInput"
+import FormAlert from "../../components/FormAlert/FormAlert"
+
+import API from "../../api"
+
+import { getItem, setItem, setUserLoggedInKey } from "../../utils"
+
+interface LoginFormProps {
+  setLogin: (login: boolean) => any
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ setLogin }) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: "all",
+  })
+
+  const [rememberMe, setRememberMe] = useState(getItem("rememberMe"))
+  const [userLoginSuccessfull, setUserLoginSuccessfull] = useState(false)
+  const [invalidCredentialsError, setInvalidCredentialsError] = useState(false)
+
+  useEffect(() => {
+    if (rememberMe) {
+      const emailToRemember = getItem("rememberMeEmail") || null
+      if (emailToRemember) {
+        setValue("email", emailToRemember)
+      }
+    } else {
+      setItem("rememberMeEmail", null)
+    }
+  })
+
+  const validSubmit = (data: any) => {
+    const email = data.email
+    const password = data.password
+    const userCredentials = { email, password }
+    console.log(userCredentials)
+
+    API.login(
+      userCredentials,
+      (res: any) => {
+        console.log(res.error)
+        return setInvalidCredentialsError(true)
+      },
+      (res: any) => {
+        console.log(res)
+
+        // Save token in localStorage
+        const token = res.token
+        console.log(token)
+        setItem("token", token)
+
+        const currentUser = res.data.filter((user: any) => {
+          return user.email === email
+        })[0]
+        const currentUserUsername = currentUser.username
+        console.log(currentUserUsername)
+
+        if (rememberMe) {
+          setItem("rememberMe", true)
+          setItem("rememberMeEmail", email)
+          setUserLoggedInKey()
+        } else {
+          setItem("rememberMe", false)
+        }
+
+        setItem("currentUser", currentUserUsername)
+        setUserLoginSuccessfull(true)
+        console.log(setLogin)
+        console.log(typeof setLogin)
+        setTimeout(() => setLogin(true), 500)
+      }
+    )
+  }
+
+  const invalidSubmit = (data: any) => {
+    setInvalidCredentialsError(true)
+  }
+
+  const handleRememberMeClick = () => {
+    setRememberMe((prevState: any) => !prevState)
+  }
+
+  return (
+    <>
+      <FormAlert
+        displayAlert={userLoginSuccessfull || invalidCredentialsError}
+        msgs={["Login successfull!", "Email or password invalid."]}
+        success={userLoginSuccessfull}
+      />
+
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card mt-5">
+            <div className="card-body" data-cy="login-form">
+              <div
+                className="alert text-center"
+                style={{
+                  backgroundColor: "#ccf3e9",
+                  color: "#00654c",
+                  borderColor: "#b8eee0",
+                }}
+              >
+                {"You need to login to continue."}
+              </div>
+              <form onSubmit={handleSubmit(validSubmit, invalidSubmit)}>
+                <div className="form-group">
+                  <LabeledInput
+                    autoFocus={true}
+                    placeholder={"Email"}
+                    registerLabel="email"
+                    register={register}
+                    // className="text-dark"
+                    required
+                    errors={errors}
+                    errorLabel={"An email address is required."}
+                    dataCy="user-email"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <LabeledInput
+                    type="password"
+                    placeholder={"Password"}
+                    registerLabel="password"
+                    register={register}
+                    required
+                    errors={errors}
+                    errorLabel={"A password is required."}
+                    dataCy="user-password"
+                  />
+                </div>
+
+                <div className="form-check mb-3">
+                  <input
+                    type="checkbox"
+                    id="user_remember_me"
+                    className="form-check-input"
+                    data-cy="remember-me-button"
+                    defaultChecked={rememberMe}
+                    onClick={handleRememberMeClick}
+                  />
+                  <label
+                    className="form-check-label float-start"
+                    data-cy="remember-me"
+                    htmlFor="user_remember_me"
+                  >
+                    {"Remember Me"}
+                  </label>
+                </div>
+
+                <div className="form-group">
+                  <button
+                    type="submit"
+                    id="login_button"
+                    className="btn btn-lg btn-outline-primary w-100 mb-2"
+                    data-cy="login-button"
+                  >
+                    {"LOGIN"}
+                  </button>
+
+                  <button
+                    id="create_account_button"
+                    className="btn btn-outline-secondary w-100"
+                    data-cy="register-link"
+                    onClick={() => {
+                      window.location.href = "/account/register"
+                    }}
+                  >
+                    {"CREATE ACCOUNT"}
+                  </button>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="">
+                    <a href="" className="text-leo-blue">
+                      {"Forgot your password?"}
+                    </a>
+                  </label>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LoginForm
---
+++ front-end/src/forms/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,198 @@
+import React, { useState } from "react"
+import LabeledInput from "../../components/LabeledInput/LabeledInput"
+import FormAlert from "../../components/FormAlert/FormAlert"
+
+import API from "../../api"
+
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import YupPassword from "yup-password"
+
+YupPassword(yup)
+
+const RegisterForm = () => {
+  const schema = yup.object({
+    username: yup
+      .string()
+      .required("A username is required.")
+      .min(6, "Username must be at least 6 characters."),
+    email: yup
+      .string()
+      .required("An email address is required.")
+      .email("Email must be  valid."),
+    password: yup
+      .string()
+      .required("A password is required.")
+      .password()
+      .minLowercase(1, "Password must contain at least 1 lowercase character")
+      .minUppercase(1, "Password must contain at least 1 uppercase character")
+      .minNumber(1, "Password must contain at least 1 number character")
+      .minSymbols(1, "Password must contain at least 1 special character"),
+    "password-confirmation": yup
+      .string()
+      .required("A password confirmation is required."),
+
+    // .min(8, "Password must contain at least 8 characters.")
+    // .matches(
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+    //   "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character."
+    // ),
+  })
+
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: "all",
+    resolver: yupResolver(schema),
+  })
+
+  const [userRegisteredSuccessfully, setUserRegisteredSuccessfully] =
+    useState(false)
+  const [invalidCredentialsError, setInvalidCredentialsError] = useState(false)
+  const [alertMessageText, setAlertMessageText] = useState("")
+
+  const [passwordsNotMatchError, setPasswordsNotMatchError] = useState(false)
+
+  const validSubmit = (data: any) => {
+    console.log(data)
+
+    const username = data.username
+    const email = data.email
+    const password = data.password
+
+    const newUserCredentials = { username, email, password }
+
+    API.register(
+      newUserCredentials,
+      (res: any) => {
+        console.warn(res.error)
+        setAlertMessageText(res.error)
+        setInvalidCredentialsError(true)
+      },
+      (res: any) => {
+        console.warn(res.message)
+        setAlertMessageText(res.message)
+
+        console.log("User registered successfully!")
+        setUserRegisteredSuccessfully(true)
+
+        setTimeout(() => (window.location.href = "/account/login"), 500)
+      }
+    )
+  }
+
+  const invalidSubmit = (data: any) => {
+    console.log(data)
+    setInvalidCredentialsError(true)
+    if (getValues("password") !== getValues("password-confirmation")) {
+      setPasswordsNotMatchError(true)
+    }
+  }
+
+  return (
+    <>
+      {" "}
+      <FormAlert
+        displayAlert={userRegisteredSuccessfully || invalidCredentialsError}
+        msgs={[
+          "User successfully registered!",
+          "Username, email or password invalid.",
+        ]}
+        success={userRegisteredSuccessfully}
+        messageBypass={alertMessageText}
+      />
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card mt-5">
+            <div className="card-body" data-cy="register-form">
+              <form onSubmit={handleSubmit(validSubmit, invalidSubmit)}>
+                <LabeledInput
+                  autoFocus={true}
+                  label={"Username"}
+                  registerLabel="username"
+                  register={register}
+                  // className="text-dark"
+                  required
+                  errors={errors}
+                  errorLabel={"A username is required."}
+                  dataCy="username"
+                />
+                <LabeledInput
+                  label={"Email"}
+                  registerLabel="email"
+                  register={register}
+                  pattern={{
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Invalid email address.",
+                  }}
+                  // className="text-dark"
+                  required
+                  errors={errors}
+                  errorLabel={"An email address is required."}
+                  dataCy="email"
+                />
+                <LabeledInput
+                  type="password"
+                  label={"Password"}
+                  registerLabel="password"
+                  register={register}
+                  required
+                  errors={errors}
+                  errorLabel={"A password is required."}
+                  dataCy="password"
+                />
+                <LabeledInput
+                  type="password"
+                  label={"Password confirmation"}
+                  registerLabel="password-confirmation"
+                  register={register}
+                  required
+                  errors={
+                    passwordsNotMatchError
+                      ? {
+                          ["password-confirmation"]: {
+                            message: "Passwords do not match.",
+                          },
+                        }
+                      : errors
+                  }
+                  errorLabel={"A password confirmation is required."}
+                  dataCy="password-confirmation"
+                />
+
+                <button
+                  type="submit"
+                  id="register_button"
+                  className="btn btn-outline-primary w-100"
+                  data-cy="register-button"
+                >
+                  {"REGISTER"}
+                </button>
+
+                <div className="mb-3">
+                  <label>{"Already have an account? "}</label>
+                  <button
+                    id="login_account_button"
+                    className="btn btn-outline-secondary w-100"
+                    data-cy="login-link"
+                    onClick={() => {
+                      window.location.href = "/"
+                    }}
+                  >
+                    {"LOGIN"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RegisterForm
