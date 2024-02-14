906f9420b4f808efb07ac0ea43b2729aa249b488
Manuel <manuelpinedacabeza@gmail.com>
2024-02-14 15:34:35 +0100

    Remove middlewre

--- back-end/routes/todos/deleteTestTodos.js
+++ back-end/routes/todos/deleteTestTodos.js
@@ -1,22 +1,17 @@
 const express = require("express")
 const router = express.Router()
 const db = require("../../database")
-const { authenticateToken } = require("../../utils")
 
-router.delete(
-  "/api/todos/delete-test-todos",
-  authenticateToken,
-  (req, res, next) => {
-    db.run(
-      `DELETE FROM Todo WHERE description LIKE '%test%'`,
-      function (err, result) {
-        if (err) {
-          return res.status(400).json({ error: res.message })
-        }
-        res.status(204).end()
+router.delete("/api/todos/delete-test-todos", (req, res, next) => {
+  db.run(
+    `DELETE FROM Todo WHERE description LIKE '%test%'`,
+    function (err, result) {
+      if (err) {
+        return res.status(400).json({ error: res.message })
       }
-    )
-  }
-)
+      res.status(204).end()
+    }
+  )
+})
 
 module.exports = router
