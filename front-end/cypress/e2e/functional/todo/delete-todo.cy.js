f706155e31592b3a98792f45e4bf8950fb8873fa
Manuel <manuelpinedacabeza@gmail.com>
2024-02-19 16:40:02 +0100

    Added todo data from fixtures

--- front-end/cypress/e2e/functional/todo/delete-todo.cy.js
+++ front-end/cypress/e2e/functional/todo/delete-todo.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 
+const validTodo = require("../../../fixtures/todo.json")
+
 describe("Todo Section - Delete Todo", () => {
-  const todoTextToDelete = "Todo to Delete (test)"
   const ctx = {}
 
   beforeEach(() => {
@@ -15,25 +16,25 @@ describe("Todo Section - Delete Todo", () => {
 
     // Visit the app or the specific page
     cy.visit("/")
-    cy.getBySel("todos-input").type(todoTextToDelete + "{enter}")
+    cy.getBySel("todo-input").type(validTodo.validTodoDesc + "{enter}")
   })
 
   // Positive tests
 
   it("should delete a todo and remove it from the list", () => {
     // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
-    cy.contains(todoTextToDelete)
+    cy.contains(validTodo.validTodoDesc)
       .parent()
       .find(".remove-icon-container")
       .click()
 
     // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
-    cy.contains(todoTextToDelete).should("not.exist")
+    cy.contains(validTodo.validTodoDesc).should("not.exist")
   })
 
   it("should not show deleted todos after page reload", () => {
     // Find the todo item with text 'Todo to Delete' and locate its delete button, then click it
-    cy.contains(todoTextToDelete)
+    cy.contains(validTodo.validTodoDesc)
       .parent()
       .find(".remove-icon-container")
       .click()
@@ -41,11 +42,11 @@ describe("Todo Section - Delete Todo", () => {
     cy.reload()
 
     // Verify that the todo with text 'Todo to Delete' no longer exists in the todo list
-    cy.contains(todoTextToDelete).should("not.exist")
+    cy.contains(validTodo.validTodoDesc).should("not.exist")
   })
 
   afterEach(() => {
-    cy.deleteTestUsers(ctx.token)
-    cy.deleteTestTodos(ctx.token)
+    cy.deleteTestUsers()
+    cy.deleteTestTodos()
   })
 })
