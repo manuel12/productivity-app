34ab69211c1d5a683a233eecd091cf491ae7b8b2
Manuel <manuelpinedacabeza@gmail.com>
2024-02-19 16:39:51 +0100

    Added todo data from fixtures

--- front-end/cypress/e2e/functional/todo/edit-todo.cy.js
+++ front-end/cypress/e2e/functional/todo/edit-todo.cy.js
@@ -1,11 +1,9 @@
 /// <reference types="cypress" />
 
+const validTodo = require("../../../fixtures/todo.json")
+const invalidTodo = require("../../../fixtures/invalidTodo.json")
+
 describe("Todo Section - Edit Todo", () => {
-  const createdTodo = "Created Todo (test)"
-  const updatedTodo = "Updated Todo Item (test)"
-  const todoTextLongerThan40Char =
-    "(test) This is a todo item with more than 40 characters, which should not be allowed"
-  const todoTextShorterThan3Char = "ab"
   const ctx = {}
 
   beforeEach(() => {
@@ -19,43 +17,51 @@ describe("Todo Section - Edit Todo", () => {
 
     // Visit the app or the specific page
     cy.visit("/")
-    cy.getBySel("todos-input").type(createdTodo + "{enter}")
+    cy.getBySel("todo-input").type(validTodo.validTodoDesc + "{enter}")
   })
 
   // Positive tests
 
   it("should edit a todo", () => {
     // Find and edit that existing todo
-    cy.getBySel("todos-item")
-      .filter(`:contains(${createdTodo})`)
+    cy.getBySel("todo-item")
+      .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
     cy.getBySel("todos-text-input").clear()
-    cy.getBySel("todos-text-input").type(`${updatedTodo}{enter}`)
+    cy.getBySel("todos-text-input").type(
+      `${validTodo.validTodoUpdateDesc}{enter}`
+    )
 
     // Validate the todo is updated correctly
-    cy.contains(createdTodo).should("not.exist")
-    cy.contains("[data-cy=todos-item]", updatedTodo).should("exist")
+    cy.contains(validTodo.validTodoDesc).should("not.exist")
+    cy.contains("[data-cy=todo-item]", validTodo.validTodoUpdateDesc).should(
+      "exist"
+    )
   })
 
   it("should persist edited todos after page reload", () => {
     // Find and edit that existing todo
-    cy.getBySel("todos-item")
-      .filter(`:contains(${createdTodo})`)
+    cy.getBySel("todo-item")
+      .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
     cy.getBySel("todos-text-input").clear()
-    cy.getBySel("todos-text-input").type(`${updatedTodo}{enter}`)
+    cy.getBySel("todos-text-input").type(
+      `${validTodo.validTodoUpdateDesc}{enter}`
+    )
 
     cy.reload()
 
     // Validate the todo is updated correctly
-    cy.contains(createdTodo).should("not.exist")
-    cy.contains("[data-cy=todos-item]", updatedTodo).should("exist")
+    cy.contains(validTodo.validTodoDesc).should("not.exist")
+    cy.contains("[data-cy=todo-item]", validTodo.validTodoUpdateDesc).should(
+      "exist"
+    )
   })
 
   // Negative tests
@@ -64,14 +70,16 @@ describe("Todo Section - Edit Todo", () => {
     cy.getBySel("todos-list").should("have.length", 1)
 
     // Find and edit that existing todo
-    cy.getBySel("todos-item")
-      .filter(`:contains(${createdTodo})`)
+    cy.getBySel("todo-item")
+      .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
     cy.getBySel("todos-text-input").clear()
-    cy.getBySel("todos-text-input").type(`${todoTextLongerThan40Char}{enter}`)
+    cy.getBySel("todos-text-input").type(
+      `${invalidTodo.todoDescLongerThan40Chars}{enter}`
+    )
 
     // Check that todo text input does not dissapear
     cy.getBySel("todos-text-input").should("be.visible")
@@ -81,33 +89,37 @@ describe("Todo Section - Edit Todo", () => {
     cy.getBySel("todos-list").should("have.length", 1)
 
     // Find and edit that existing todo
-    cy.getBySel("todos-item")
-      .filter(`:contains(${createdTodo})`)
+    cy.getBySel("todo-item")
+      .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
     cy.getBySel("todos-text-input").clear()
-    cy.getBySel("todos-text-input").type(`${todoTextLongerThan40Char}{enter}`)
+    cy.getBySel("todos-text-input").type(
+      `${invalidTodo.todoDescLongerThan40Chars}{enter}`
+    )
 
     // Check error label
     cy.getBySel("todo-error-label")
       .should("be.visible")
-      .and("contain.text", "Todo cannot contain more than 40 characters")
+      .and("contain.text", "Todos cannot contain more than 40 characters.")
   })
 
   it("should not allow to edit a todo so it has less than 3 characters", () => {
     cy.getBySel("todos-list").should("have.length", 1)
 
     // Find and edit that existing todo
-    cy.getBySel("todos-item")
-      .filter(`:contains(${createdTodo})`)
+    cy.getBySel("todo-item")
+      .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
     cy.getBySel("todos-text-input").clear()
-    cy.getBySel("todos-text-input").type(`${todoTextShorterThan3Char}{enter}`)
+    cy.getBySel("todos-text-input").type(
+      `${invalidTodo.todoDescShorterThan3Chars}{enter}`
+    )
 
     // Check that todo text input does not dissapear
     cy.getBySel("todos-text-input").should("be.visible")
@@ -117,23 +129,25 @@ describe("Todo Section - Edit Todo", () => {
     cy.getBySel("todos-list").should("have.length", 1)
 
     // Find and edit that existing todo
-    cy.getBySel("todos-item")
-      .filter(`:contains(${createdTodo})`)
+    cy.getBySel("todo-item")
+      .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
     cy.getBySel("todos-text-input").clear()
-    cy.getBySel("todos-text-input").type(`${todoTextShorterThan3Char}{enter}`)
+    cy.getBySel("todos-text-input").type(
+      `${invalidTodo.todoDescShorterThan3Chars}{enter}`
+    )
 
     // Check error label
     cy.getBySel("todo-error-label")
       .should("be.visible")
-      .and("contain.text", "Todo cannot contain less than 3 characters")
+      .and("contain.text", "Todos cannot contain less than 3 characters.")
   })
 
   afterEach(() => {
-    cy.deleteTestUsers(ctx.token)
-    cy.deleteTestTodos(ctx.token)
+    cy.deleteTestUsers()
+    cy.deleteTestTodos()
   })
 })
