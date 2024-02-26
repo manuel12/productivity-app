1976600c462f1c2e6f2797439fdd31a67ed76a87
Manuel <manuelpinedacabeza@gmail.com>
2024-02-26 16:17:44 +0100

    Addded aria elements

--- front-end/src/components/CustomList/CustomList.tsx
+++ front-end/src/components/CustomList/CustomList.tsx
@@ -16,16 +16,18 @@ const CustomList: React.FC<CustomListProps> = ({
 }) => {
   return (
     <div className="mx-auto mb-5">
-      <ul
-        className="list-group mx-auto CL__unordered-list"
-        data-cy={dataCyAttr}
-      >
-        {items.length < 1 ? (
-          <div className="display-4">No {itemName} added yet...</div>
-        ) : (
-          children
-        )}
-      </ul>
+      {items.length < 1 ? (
+        <div className="display-4">No {itemName} added yet...</div>
+      ) : (
+        <ul
+          className="list-group mx-auto CL__unordered-list"
+          data-cy={dataCyAttr}
+          role="list" //
+          aria-label={`List of ${itemName}`}
+        >
+          {children}
+        </ul>
+      )}
     </div>
   )
 }
