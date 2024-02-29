2a96876d3f399c1799c356a478fe549fbc064e41
Manuel <manuelpinedacabeza@gmail.com>
2024-02-29 16:22:44 +0100

    Addded aria elements

--- front-end/src/pages/RegisterPage/RegisterPage.tsx
+++ front-end/src/pages/RegisterPage/RegisterPage.tsx
@@ -3,7 +3,10 @@ import RegisterForm from "../../forms/RegisterForm/RegisterForm"
 
 const RegisterPage = () => {
   return (
-    <div>
+    <div role="main" aria-label="Register Page">
+      <h1 className="display-1" data-cy="login-page">
+        Register
+      </h1>
       <RegisterForm />
     </div>
   )
