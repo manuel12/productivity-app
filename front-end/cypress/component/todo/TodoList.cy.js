5076d63847dd745bb12066b1ae6ca5f710f3cf5a
Manuel <manuelpinedacabeza@gmail.com>
2024-03-01 17:04:50 +0100

    Added TodoList tests

---
+++ front-end/cypress/component/todo/TodoList.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+import TodoList from "../../../src/components/TodosList/TodosList"
+
+describe("TodoList", () => {
+  const todos = [
+    {
+      description: "feed the cats",
+      completed: false,
+    },
+
+    {
+      description: "take out trash",
+      completed: true,
+    },
+  ]
+
+  beforeEach(() => {
+    cy.mount(
+      <TodoList todos={todos} setTodos={null} setNumCompletedTodos={null} />
+    )
+  })
+
+  it("should display input with placeholder text 'Add new todos...'", () => {
+    cy.get('[data-cy="todo-input"]')
+      .should("be.visible")
+      .and("have.attr", "placeholder")
+      .and("contain", "Add new todo...")
+  })
+
+  it("should display button with the text 'Add Todo'", () => {
+    cy.get('[data-cy="todo-submit"]')
+      .should("be.visible")
+      .and("contain.text", "Add Todo")
+  })
+})
