0483c9a5dc618e663298b24caf54f824868fe67b
Manuel <manuelpinedacabeza@gmail.com>
2024-03-02 17:07:12 +0100

    Updated TodoPage tests

--- front-end/cypress/e2e/responsiveness/todo/todo-page.cy.js
+++ front-end/cypress/e2e/responsiveness/todo/todo-page.cy.js
@@ -1,47 +1,29 @@
 /// <reference types="cypress" />
 
-describe("Responsiveness Section - Todo Page", () => {
-  const resolutions = [
-    {
-      breakpoint: "<600px",
-      viewportWidthToUse: 414,
-    },
-    {
-      breakpoint: ">600px",
-      viewportWidthToUse: 601,
-    },
-    {
-      breakpoint: ">768px",
-      viewportWidthToUse: 770,
-    },
-    {
-      breakpoint: ">992px",
-      viewportWidthToUse: 1000,
-    },
-    {
-      breakpoint: ">1200px",
-      viewportWidthToUse: 1300,
-    },
-  ]
+const resolutions = require("../../../fixtures/resolutions.json")
 
+describe("Responsiveness Section - Todo Page", () => {
   const isMobile = (resolution) => resolution.viewportWidthToUse < 992
 
   resolutions.forEach((resolution) => {
     context(`${resolution.breakpoint} resolution `, () => {
       before(() => {
         cy.viewport(resolution.viewportWidthToUse, 900)
+
         cy.visit("/")
+        cy.registerWithAPI()
+        cy.login()
       })
 
       it(`should be displayed correctly on ${resolution.breakpoint} of width`, () => {
-        cy.getBySel("app-navbar")
+        cy.getBySel("navbar")
           .should("be.visible")
           .matchImageSnapshot(`Navbar at ${resolution.viewportWidthToUse}`)
 
         if (isMobile(resolution)) {
           // Open the navbar and take snapshot of the menu
           cy.get(".navbar-toggler").click()
-          cy.getBySel("app-navbar")
+          cy.getBySel("navbar")
             .should("be.visible")
             .matchImageSnapshot(
               `Navbar (opened) at ${resolution.viewportWidthToUse}`
