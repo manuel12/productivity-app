d1da8ef32faeceda2f9938c9ec40cd10bfa415ad
Manuel <manuelpinedacabeza@gmail.com>
2024-03-04 17:16:02 +0100

    Added responsivesness  authentication tests

---
+++ front-end/cypress/e2e/responsiveness/authentication/register-page.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+const resolutions = require("../../../fixtures/resolutions.json")
+
+describe("Responsiveness Section - RegisterPage", () => {
+  resolutions.forEach((resolution) => {
+    context(`${resolution.breakpoint} resolution `, () => {
+      before(() => {
+        cy.viewport(resolution.viewportWidthToUse, 900)
+        cy.visit("/")
+        cy.getBySel("register-button").click()
+      })
+
+      it(`should be displayed correctly on ${resolution.breakpoint} of width`, () => {
+        cy.getBySel("login-page")
+          .should("be.visible")
+          .matchImageSnapshot(`LoginPage at ${resolution.viewportWidthToUse}`)
+      })
+    })
+  })
+})
