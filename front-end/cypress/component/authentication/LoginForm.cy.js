f9171bde80875756a34ec0d0ddfa08f8f3f21a35
Manuel <manuelpinedacabeza@gmail.com>
2024-03-08 09:56:49 +0100

    Added getBySel calls

--- front-end/cypress/component/authentication/LoginForm.cy.js
+++ front-end/cypress/component/authentication/LoginForm.cy.js
@@ -8,44 +8,44 @@ describe("LoginForm", () => {
   })
 
   it("should display a 'You need to login to continue.' text", () => {
-    cy.get(".alert")
+    cy.getBySel("login-alert")
       .should("be.visible")
       .and("contain.text", "You need to login to continue.")
   })
 
   it("should display an email input", () => {
-    cy.get('[data-cy="email"]')
+    cy.getBySel("email")
       .should("be.visible")
       .and("have.attr", "placeholder")
       .and("contain", "Email")
   })
 
   it("should display an password input", () => {
-    cy.get('[data-cy="password"]')
+    cy.getBySel("password")
       .should("be.visible")
       .and("have.attr", "placeholder")
       .and("contain", "Password")
   })
 
   it("should display a 'Remember Me' button", () => {
-    cy.get('[data-cy="remember-me-button"]').should("be.visible")
+    cy.getBySel("remember-me-button").should("be.visible")
 
-    cy.get('[data-cy="remember-me"]').should("be.visible")
+    cy.getBySel("remember-me").should("be.visible")
   })
 
   it("should display a 'LOGIN' button", () => {
-    cy.get('[data-cy="login-button"]').should("be.visible")
+    cy.getBySel("login-button").should("be.visible")
   })
 
   it("should display a 'CREATE ACCOUNT' button", () => {
-    cy.get('[data-cy="register-button"]').should("be.visible")
+    cy.getBySel("register-button").should("be.visible")
   })
 
   it("should have focus set on then email field", () => {
-    cy.get('[data-cy="email"]').should("have.focus")
+    cy.getBySel("email").should("have.focus")
   })
 
   it("should display a password input field of type password", () => {
-    cy.get('input[type="password"]').should("be.visible")
+    cy.getBySel("password").should("be.visible")
   })
 })
