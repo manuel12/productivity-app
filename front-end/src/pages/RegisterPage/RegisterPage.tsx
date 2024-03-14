ad70d969ea38fb68b730264cc8345c3a3cebd8d1
Manuel <manuelpinedacabeza@gmail.com>
2024-03-14 11:25:57 +0100

    Added data-cy attribute

--- front-end/src/pages/RegisterPage/RegisterPage.tsx
+++ front-end/src/pages/RegisterPage/RegisterPage.tsx
@@ -3,10 +3,8 @@ import RegisterForm from "../../forms/RegisterForm/RegisterForm"
 
 const RegisterPage = () => {
   return (
-    <div role="main" aria-label="Register Page">
-      <h1 className="display-1" data-cy="login-page">
-        Register
-      </h1>
+    <div role="main" aria-label="Register Page" data-cy="register-page">
+      <h1 className="display-1">Register</h1>
       <RegisterForm />
     </div>
   )
