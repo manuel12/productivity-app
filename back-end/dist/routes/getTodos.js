284de559888fd52962309d056e8c89c4ab8c2f76
Manuel <manuelpinedacabeza@gmail.com>
2025-02-26 11:27:23 +0100

    Updated getTodos route

--- back-end/dist/routes/getTodos.js
+++ back-end/dist/routes/getTodos.js
@@ -1,20 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const database_1 = require("../database");
-const router = express_1.default.Router();
+"use strict"
+Object.defineProperty(exports, "__esModule", { value: true })
+const express_1 = require("express")
+const database_1 = require("../database")
+const router = express_1.default.Router()
 /* GET todos listing */
 router.get("/api/todos/", function (req, res, next) {
-    const querySQL = "SELECT * FROM Todo";
-    const params = [];
-    database_1.default.all(querySQL, params, (err, row) => {
-        if (err) {
-            return res.status(400).json({ error: err.message });
-        }
-        res.json({
-            message: "Todos successfully retrieved!",
-            data: row,
-        });
-    });
-});
-exports.default = router;
+  const querySQL = "SELECT * FROM Todo"
+  const params = []
+  database_1.default.all(querySQL, params, (err, row) => {
+    if (err) {
+      return res.status(400).json({ error: err.message })
+    }
+    res.json({
+      message: "Todos successfully retrieved!",
+      data: row,
+    })
+  })
+})
+exports.default = router
