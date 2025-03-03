e3e3a036792474859682b9a9a84786df14e6ec5c
Manuel <manuelpinedacabeza@gmail.com>
2025-03-03 19:48:52 +0100

    Updated interface

--- front-end/src/interfaces/IDailyItemProps.tsx
+++
@@ -1,9 +0,0 @@
-import IDaily from "./IDaily"
-
-export default interface IDailyItemProps {
-  index: number
-  daily: IDaily
-  dailies: IDaily[]
-  setDailies: (dailies: IDaily[]) => void
-  setCompletedDailies: (completedDailies: number) => void
-}
