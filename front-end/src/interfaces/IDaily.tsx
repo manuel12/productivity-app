f4e42888364ba7575edf1a0c0763c2aaf0761f62
Manuel <manuelpinedacabeza@gmail.com>
2025-03-03 19:48:45 +0100

    Updated interface

--- front-end/src/interfaces/IDaily.tsx
+++
@@ -1,8 +0,0 @@
-export default interface IDaily {
-  id?: number
-  completed: boolean
-  description: string
-  dateCreated: Date | string
-  streakCounter: number
-  lastCompletedDate?: Date | string
-}
