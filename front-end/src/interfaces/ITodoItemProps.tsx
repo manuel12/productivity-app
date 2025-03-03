73e18aed7b15f4c0cd1bb86ed6618a21f884154d
Manuel <manuelpinedacabeza@gmail.com>
2025-03-03 19:49:16 +0100

    Updated interface

--- front-end/src/interfaces/ITodoItemProps.tsx
+++
@@ -1,9 +0,0 @@
-import ITodo from "./ITodo"
-
-export default interface ITodoItemProps {
-  index: number
-  todo: ITodo
-  todos: ITodo[]
-  setTodos: (todos: ITodo[]) => void
-  setNumCompletedTodos: (numCompletedTodos: number) => void
-}
