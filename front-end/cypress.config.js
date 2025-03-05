48fd77cef8a1a7fed8c252d4a744daca7fa30070
Manuel <manuelpinedacabeza@gmail.com>
2025-03-05 13:36:11 +0100

    Added mochaawesome reporter and options, added a11y plugin

--- front-end/cypress.config.js
+++ front-end/cypress.config.js
@@ -2,15 +2,29 @@ const { defineConfig } = require("cypress")
 const {
   addMatchImageSnapshotPlugin,
 } = require("@simonsmith/cypress-image-snapshot/plugin")
+const addAccessibilityTasks = require("wick-a11y/accessibility-tasks")
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Productivity App - Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       addMatchImageSnapshotPlugin(on)
+      require("cypress-mochawesome-reporter/plugin")(on)
+      addAccessibilityTasks(on)
     },
     baseUrl: "http://localhost:3000/",
     specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    excludeSpecPattern:
+      "cypress/tests/functional/daily/**/*.cy.{js,jsx,ts,tsx}",
     experimentalRunAllSpecs: true,
     screenshotOnRunFailure: false,
     screenshotsFolder: false,
