0ae923e499a9e15c58a8f23f3edc7d28b4596d39
Manuel <manuelpinedacabeza@gmail.com>
2025-03-07 13:46:04 +0100

    Updated component

--- front-end/src/pages/RegisterPage/RegisterPage.tsx
+++ front-end/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,9 +1,21 @@
 import RegisterForm from "../../forms/RegisterForm/RegisterForm"
 
 const RegisterPage = () => {
+  const handleClick = () => {
+    window.location.reload()
+  }
+
   return (
     <div role="main" aria-label="Register Page" data-cy="register-page">
-      <h1 className="display-1">Register</h1>
+      <h1
+        className="display-1"
+        onClick={handleClick}
+        style={{
+          cursor: "pointer",
+        }}
+      >
+        Register
+      </h1>
       <RegisterForm />
     </div>
   )
