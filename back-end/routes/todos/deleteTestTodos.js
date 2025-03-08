66a8208440db432c73ec05618625018cb856942f
Manuel <manuelpinedacabeza@gmail.com>
2025-03-08 13:37:47 +0100

    Updated route

--- back-end/routes/todos/deleteTestTodos.js
+++ back-end/routes/todos/deleteTestTodos.js
@@ -1,17 +1,36 @@
 const express = require("express")
 const router = express.Router()
-const db = require("../../database")
+const Todo = require("../../models/Todo") // Import your Sequelize Todo model
+const { Op } = require("sequelize") // Import Sequelize operators
 
-router.delete("/api/todos/delete-test-todos", (req, res, next) => {
-  db.run(
-    `DELETE FROM Todo WHERE description LIKE '%test%'`,
-    function (err, result) {
-      if (err) {
-        return res.status(400).json({ error: res.message })
-      }
-      res.status(204).end()
+router.delete("/api/todos/delete-test-todos", async (req, res) => {
+  try {
+    // Delete all todos where the description contains "test"
+    const deletedTodos = await Todo.destroy({
+      where: {
+        description: {
+          [Op.like]: "%test%", // Use Op.like for case-insensitive substring matching
+        },
+      },
+    })
+
+    if (deletedTodos > 0) {
+      // At least one todo was deleted
+      return res
+        .status(204)
+        .json({ message: `${deletedTodos} todos containing "test" deleted.` })
+    } else {
+      // No todos matched the criteria
+      return res
+        .status(204)
+        .json({ message: "No todos containing 'test' found." })
     }
-  )
+  } catch (err) {
+    console.error(err)
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the test todos." })
+  }
 })
 
 module.exports = router
