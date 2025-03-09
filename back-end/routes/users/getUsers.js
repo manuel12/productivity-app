9c3b20a86c8be52ef3fdc059fc5b738e37631f38
Manuel <manuelpinedacabeza@gmail.com>
2025-03-09 13:40:03 +0100

    Updated route

--- back-end/routes/users/getUsers.js
+++ back-end/routes/users/getUsers.js
@@ -1,21 +1,20 @@
 const express = require("express")
 const router = express.Router()
-const db = require("../../database")
+const User = require("../../models/User") // Import your Sequelize User model
 
-/* GET users listing. */
-router.get("/api/users", function (req, res, next) {
-  const selectSQL = "select * from User"
-  const params = []
-  db.all(selectSQL, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message })
-      return
-    }
-    res.json({
+router.get("/api/users", async (req, res) => {
+  try {
+    // Fetch all users using Sequelize
+    const users = await User.findAll()
+
+    res.status(200).json({
       message: "Users successfully retrieved!",
-      data: rows,
+      data: users,
     })
-  })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "An error occurred while retrieving users." })
+  }
 })
 
 module.exports = router
