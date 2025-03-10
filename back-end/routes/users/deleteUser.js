01fa75419aa1bb09b348d37d583526ca09c907a1
Manuel <manuelpinedacabeza@gmail.com>
2025-03-10 13:41:46 +0100

    Updated route

--- back-end/routes/users/deleteUser.js
+++ back-end/routes/users/deleteUser.js
@@ -1,22 +1,30 @@
 const express = require("express")
 const router = express.Router()
-const db = require("../../database")
+const User = require("../../models/User") // Import your Sequelize User model
 
-router.delete("/api/user/:id", (req, res, next) => {
-  db.run(
-    "DELETE FROM User WHERE id = ?",
-    req.params.id,
-    function (err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message })
-        return
-      }
-      return res.json({
-        message: "User successfully deleted!",
-        rows: this.changes,
-      })
+router.delete("/api/user/:id", async (req, res) => {
+  const id = req.params.id
+
+  try {
+    // Delete the user with the specified ID
+    const result = await User.destroy({ where: { id } })
+
+    // If no user was deleted, return a 404
+    if (result === 0) {
+      return res.status(404).json({ error: `User with id ${id} not found.` })
     }
-  )
+
+    // Respond with a success message
+    res.status(200).json({
+      message: "User successfully deleted!",
+      rows: result, // Number of rows deleted
+    })
+  } catch (err) {
+    console.error(err)
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the user." })
+  }
 })
 
 module.exports = router
