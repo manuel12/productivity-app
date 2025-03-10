ff8cbaa5f2c813f74cecacddebb4ef3b0ebec4e1
Manuel <manuelpinedacabeza@gmail.com>
2025-03-10 13:44:21 +0100

    Added test comments and steps

--- front-end/cypress/tests/functional/todo/edit-todo.cy.js
+++ front-end/cypress/tests/functional/todo/edit-todo.cy.js
@@ -23,42 +23,71 @@ describe("Todo Section - Edit Todo", () => {
   // Positive tests
 
   it("should edit a todo", () => {
-    // Find and edit that existing todo
+    // 1. Click on the todo's description text to turn it into an input
+    cy.step("Click on the todo's description text to turn it into an input")
     cy.getBySel("todo-item")
       .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
+    // 2. Delete the current description
+    cy.step("Delete the current description")
     cy.getBySel("todos-text-input").clear()
+
+    // 3. Enter todoDescription on the input
+    cy.step("Enter todoDescription on the input")
+
+    // 4. Press ENTER key
+    cy.step("Press ENTER key")
+
     cy.getBySel("todos-text-input").type(
       `${validTodo.validTodoUpdateDesc}{enter}`
     )
 
-    // Validate the todo is updated correctly
+    // 5. Check the old description text is not visible in the todo
+    cy.step("Check the old description text is not visible in the todo")
     cy.contains(validTodo.validTodoDesc).should("not.exist")
+
+    // 6. Check the new description text is visible in the todo
+    cy.step("Check the new description text is visible in the todo")
     cy.contains("[data-cy=todo-item]", validTodo.validTodoUpdateDesc).should(
       "exist"
     )
   })
 
   it("should persist edited todos after page reload", () => {
-    // Find and edit that existing todo
+    // 1. Click on the todo's description text to turn it into an input
+    cy.step("Click on the todo's description text to turn it into an input")
     cy.getBySel("todo-item")
       .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
+    // 2. Delete the current description
+    cy.step("Delete the current description")
     cy.getBySel("todos-text-input").clear()
+
+    // 3. Enter todoDescription on the input
+    cy.step("Enter todoDescription on the input")
+
+    // 4. Press ENTER key
+    cy.step("Press ENTER key")
     cy.getBySel("todos-text-input").type(
       `${validTodo.validTodoUpdateDesc}{enter}`
     )
 
+    // 5. Reload the page
+    cy.step("Reload the page")
     cy.reload()
 
-    // Validate the todo is updated correctly
+    // 6. Check the old description text is not visible in the todo
+    cy.step("Check the old description text is not visible in the todo")
     cy.contains(validTodo.validTodoDesc).should("not.exist")
+
+    // 7. Check the new description text is visible in the todo
+    cy.step("Check the new description text is visible in the todo")
     cy.contains("[data-cy=todo-item]", validTodo.validTodoUpdateDesc).should(
       "exist"
     )
@@ -67,80 +96,119 @@ describe("Todo Section - Edit Todo", () => {
   // Negative tests
 
   it("should not allow to edit a todo so it has more than 40 characters", () => {
-    cy.getBySel("todos-list").should("have.length", 1)
-
-    // Find and edit that existing todo
+    // 1. Click on the todo's description text to turn it into an input
+    cy.step("Click on the todo's description text to turn it into an input")
     cy.getBySel("todo-item")
       .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
+    // 2. Delete the current description
+    cy.step("Delete the current description")
     cy.getBySel("todos-text-input").clear()
+
+    // 3. Enter todoDescLongerThan40Chars
+    cy.step("Enter todoDescLongerThan40Chars")
+
+    // 4. Press ENTER key
+    cy.step("Press ENTER key")
+
     cy.getBySel("todos-text-input").type(
       `${invalidTodo.todoDescLongerThan40Chars}{enter}`
     )
 
-    // Check that todo text input does not dissapear
+    // 5. Check the todo text input does not disappear
+    cy.step("Check the todo text input does not disappear")
     cy.getBySel("todos-text-input").should("be.visible")
   })
 
   it('should display an error label "Todos must be shorter than 40 characters." when edited todo exceeds that amount', () => {
-    cy.getBySel("todos-list").should("have.length", 1)
-
-    // Find and edit that existing todo
+    // 1. Click on the todo's description text to turn it into an input
+    cy.step("Click on the todo's description text to turn it into an input")
     cy.getBySel("todo-item")
       .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
+    // 2. Delete the current description
+    cy.step("Delete the current description")
     cy.getBySel("todos-text-input").clear()
+
+    // 3. Enter todoDescLongerThan40Chars
+    cy.step("Enter todoDescLongerThan40Chars")
+
+    // 4. Press ENTER key
+    cy.step("Press ENTER key")
+
     cy.getBySel("todos-text-input").type(
       `${invalidTodo.todoDescLongerThan40Chars}{enter}`
     )
 
-    // Check error label
+    // 5. Check an error label for the todo input with the text 'Todos cannot contain more than 40 characters.' is visible
+    cy.step(
+      "Check an error label for the todo input with the text 'Todos cannot contain more than 40 characters.' is visible"
+    )
     cy.getBySel("todo-error-label")
       .should("be.visible")
       .and("contain.text", "Todos cannot contain more than 40 characters.")
   })
 
   it("should not allow to edit a todo so it has less than 3 characters", () => {
-    cy.getBySel("todos-list").should("have.length", 1)
-
-    // Find and edit that existing todo
+    // 1. Click on the todo's description text to turn it into an input
+    cy.step("Click on the todo's description text to turn it into an input")
     cy.getBySel("todo-item")
       .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
+    // 2. Delete the current description
+    cy.step("Delete the current description")
     cy.getBySel("todos-text-input").clear()
+
+    // 3. Enter shortDescription
+    cy.step("Enter shortDescription")
+
+    // 4. Press ENTER key
+    cy.step("Press ENTER key")
     cy.getBySel("todos-text-input").type(
       `${invalidTodo.todoDescShorterThan3Chars}{enter}`
     )
-
-    // Check that todo text input does not dissapear
+    // 5. Check the todo text input does not disappear// Check that todo text input does not dissapear
+    cy.step(
+      "Check the todo text input does not disappear// Check that todo text input does not dissapear"
+    )
     cy.getBySel("todos-text-input").should("be.visible")
   })
 
   it('should display an error label "Todos must be at least 3 characters." when edited todo falls below that amount', () => {
-    cy.getBySel("todos-list").should("have.length", 1)
-
-    // Find and edit that existing todo
+    // 1. Click on the todo's description text to turn it into an input
+    cy.step("Click on the todo's description text to turn it into an input")
     cy.getBySel("todo-item")
       .filter(`:contains(${validTodo.validTodoDesc})`)
       .within(() => {
         cy.getBySel("todos-description-container").click()
       })
 
+    // 2. Delete the current description
+    cy.step("Delete the current description")
     cy.getBySel("todos-text-input").clear()
+
+    // 3. Enter shortDescription
+    cy.step("Enter shortDescription")
+
+    // 4. Press ENTER key
+    cy.step("Press ENTER key")
     cy.getBySel("todos-text-input").type(
       `${invalidTodo.todoDescShorterThan3Chars}{enter}`
     )
 
-    // Check error label
+    // 5. Check an error label for the todo input with the text 'Todos cannot contain less than 3 characters.' is visible
+    cy.step(
+      "Check an error label for the todo input with the text 'Todos cannot contain less than 3 characters.' is visible"
+    )
     cy.getBySel("todo-error-label")
       .should("be.visible")
       .and("contain.text", "Todos must be at least 3 characters.")
