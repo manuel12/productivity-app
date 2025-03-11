1754bf193ada2c10885d4777c0635d9a6c331dbd
Manuel <manuelpinedacabeza@gmail.com>
2025-03-11 13:47:05 +0100

    Updated LoginPage componenmt

--- front-end/src/pages/LoginPage/LoginPage.tsx
+++ front-end/src/pages/LoginPage/LoginPage.tsx
@@ -6,9 +6,21 @@ interface LoggedOutRoutesProps {
 }
 
 const LoginPage: React.FC<LoggedOutRoutesProps> = ({ setLogin }) => {
+  const handleClick = () => {
+    window.location.reload()
+  }
+
   return (
     <div role="main" aria-label="Login Page" data-cy="login-page">
-      <h1 className="display-1">Login</h1>
+      <h1
+        className="display-1"
+        onClick={handleClick}
+        style={{
+          cursor: "pointer",
+        }}
+      >
+        Login
+      </h1>
       <LoginForm setLogin={setLogin} />
     </div>
   )
