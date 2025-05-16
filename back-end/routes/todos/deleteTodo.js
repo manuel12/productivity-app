1b8e7aa286018ec722a4ed286738a517ecd24441
Manuel <manuelpinedacabeza@gmail.com>
2025-05-16 20:06:33 +0200

    Updated route

--- back-end/routes/todos/deleteTodo.js
+++ back-end/routes/todos/deleteTodo.js
@@ -3,33 +3,37 @@ const router = express.Router()
 const { authenticateToken } = require("../../utils")
 const Todo = require("../../models/Todo") // Import your Sequelize Todo model
 
-router.delete("/api/todo/:id", authenticateToken, async (req, res) => {
-  const id = Number(req.params.id)
+router.delete(
+  "/api/todo/:id",
+  //authenticateToken,
+  async (req, res) => {
+    const id = Number(req.params.id)
 
-  // Validate the ID
-  if (isNaN(id) || id <= 0) {
-    return res.status(400).json({ error: "Invalid (NaN) todo id" })
-  }
-
-  try {
-    // Check if the Todo exists
-    const todo = await Todo.findOne({ where: { id } })
-
-    if (!todo) {
-      return res.status(404).json({ error: `Todo with id ${id} not found` })
+    // Validate the ID
+    if (isNaN(id) || id <= 0) {
+      return res.status(400).json({ error: "Invalid (NaN) todo id" })
     }
 
-    // Delete the Todo
-    await todo.destroy()
+    try {
+      // Check if the Todo exists
+      const todo = await Todo.findOne({ where: { id } })
 
-    // Respond with a 204 status (No Content)
-    return res.status(204).end()
-  } catch (err) {
-    console.error(err)
-    res
-      .status(500)
-      .json({ error: "An error occurred while deleting the todo." })
+      if (!todo) {
+        return res.status(404).json({ error: `Todo with id ${id} not found` })
+      }
+
+      // Delete the Todo
+      await todo.destroy()
+
+      // Respond with a 204 status (No Content)
+      return res.status(204).end()
+    } catch (err) {
+      console.error(err)
+      res
+        .status(500)
+        .json({ error: "An error occurred while deleting the todo." })
+    }
   }
-})
+)
 
 module.exports = router
