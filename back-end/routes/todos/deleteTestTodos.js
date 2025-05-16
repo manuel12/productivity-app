1cd956c0699f1afbbafc87bf7345bcb1b5936745
Manuel <manuelpinedacabeza@gmail.com>
2025-05-16 20:06:09 +0200

    Updated route

--- back-end/routes/todos/deleteTestTodos.js
+++ back-end/routes/todos/deleteTestTodos.js
@@ -1,35 +1,39 @@
 const express = require("express")
 const router = express.Router()
+const { authenticateToken } = require("../../utils")
 const Todo = require("../../models/Todo") // Import your Sequelize Todo model
-const { Op } = require("sequelize") // Import Sequelize operators
 
-router.delete("/api/todos/delete-test-todos", async (req, res) => {
-  try {
-    // Delete all todos where the description contains "test"
-    const deletedTodosCount = await Todo.destroy({
-      where: {}, // An empty where clause or omitting it altogether deletes all records
-      truncate: true, // This is a more efficient way to delete all records in some databases (e.g., PostgreSQL, MySQL)
-      // It effectively resets the table, including auto-incrementing IDs.
-      // Be cautious with this if you need to preserve IDs or related data.
-    })
-
-    if (deletedTodosCount > 0) {
-      // At least one todo was deleted
-      return res.status(204).json({
-        message: `${deletedTodosCount} todos containing deleted.`,
+router.delete(
+  "/api/todos/delete-test-todos",
+  //authenticateToken,
+  async (req, res) => {
+    try {
+      // Delete all todos where the description contains "test"
+      const deletedTodosCount = await Todo.destroy({
+        where: {}, // An empty where clause or omitting it altogether deletes all records
+        truncate: true, // This is a more efficient way to delete all records in some databases (e.g., PostgreSQL, MySQL)
+        // It effectively resets the table, including auto-incrementing IDs.
+        // Be cautious with this if you need to preserve IDs or related data.
       })
-    } else {
-      // No todos matched the criteria
-      return res
-        .status(204)
-        .json({ message: "No todos containing 'test' found." })
+
+      if (deletedTodosCount > 0) {
+        // At least one todo was deleted
+        return res.status(204).json({
+          message: `${deletedTodosCount} todos containing deleted.`,
+        })
+      } else {
+        // No todos matched the criteria
+        return res
+          .status(204)
+          .json({ message: "No todos containing 'test' found." })
+      }
+    } catch (err) {
+      console.error(err)
+      res
+        .status(500)
+        .json({ error: "An error occurred while deleting the test todos." })
     }
-  } catch (err) {
-    console.error(err)
-    res
-      .status(500)
-      .json({ error: "An error occurred while deleting the test todos." })
   }
-})
+)
 
 module.exports = router
